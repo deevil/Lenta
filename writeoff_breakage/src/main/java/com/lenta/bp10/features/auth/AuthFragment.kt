292017a363b6c6afff7954ad5c0a5d06387fa9f3
Denis Borisnko <borisenkoda@gmail.com>
2019-04-17 11:54:31 +0300

    top bar buttons for auth screen

--- writeoff_breakage/src/main/java/com/lenta/bp10/features/auth/AuthFragment.kt
+++ writeoff_breakage/src/main/java/com/lenta/bp10/features/auth/AuthFragment.kt
@@ -4,6 +4,7 @@ import com.lenta.bp10.R
 import com.lenta.bp10.platform.extentions.getAppComponent
 import com.lenta.shared.features.login.CoreAuthViewModel
 import com.lenta.shared.features.login.CoreLoginFragment
+import com.lenta.shared.platform.toolbar.top_toolbar.ImageButtonDecorationInfo
 import com.lenta.shared.platform.toolbar.top_toolbar.TopToolbarUiModel
 import com.lenta.shared.utilities.extentions.provideViewModel
 
@@ -19,6 +20,8 @@ class AuthFragment : CoreLoginFragment() {
     override fun setupTopToolBar(topToolbarUiModel: TopToolbarUiModel) {
         topToolbarUiModel.title.value = getString(R.string.app_title)
         topToolbarUiModel.description.value = getString(R.string.authorization)
+        topToolbarUiModel.uiModelButton1.show(ImageButtonDecorationInfo.settings)
+        topToolbarUiModel.uiModelButton2.show(ImageButtonDecorationInfo.exitFromApp)
     }
 
 
