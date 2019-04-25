274114f14edb91d220d4fc7a73e6bdade124a09b
Artem Islamov <artem.islamov@eigenmethod.com.ua>
2019-04-25 12:11:49 +0300

    delete unnecessary code

--- LENTABP10/src/main/java/com/lenta/lentabp10/models/repositories/IProcessProductService.java
+++
@@ -1,9 +0,0 @@
-package com.lenta.lentabp10.models.repositories;
-
-import com.lenta.lentabp10.models.task.WriteOffTask;
-
-public interface IProcessProductService {
-    double getTotalCount();
-    WriteOffTask apply();
-    WriteOffTask discard();
-}
