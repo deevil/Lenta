636f6b0f1715e2ab00b68709ae2e973e0735c30e
Denis Borisnko <borisenkoda@gmail.com>
2019-04-26 09:47:08 +0300

    rename goods_list_fragment.xml to fragment_goods_list.xml with new template

--- writeoff_breakage/src/main/java/com/lenta/bp10/features/goods_list/GoodsListFragment.kt
+++ writeoff_breakage/src/main/java/com/lenta/bp10/features/goods_list/GoodsListFragment.kt
@@ -1,16 +1,16 @@
 package com.lenta.bp10.features.goods_list
 
 import com.lenta.bp10.R
-import com.lenta.bp10.databinding.GoodsListFragmentBinding
+import com.lenta.bp10.databinding.FragmentGoodsListBinding
 import com.lenta.bp10.platform.extentions.getAppComponent
 import com.lenta.shared.platform.fragment.CoreFragment
 import com.lenta.shared.platform.toolbar.bottom_toolbar.BottomToolbarUiModel
 import com.lenta.shared.platform.toolbar.top_toolbar.TopToolbarUiModel
 import com.lenta.shared.utilities.extentions.provideViewModel
 
-class GoodsListFragment : CoreFragment<GoodsListFragmentBinding, GoodsListViewModel>() {
+class GoodsListFragment : CoreFragment<FragmentGoodsListBinding, GoodsListViewModel>() {
 
-    override fun getLayoutId(): Int = R.layout.goods_list_fragment
+    override fun getLayoutId(): Int = R.layout.fragment_goods_list
 
     override fun getPageNumber(): String {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
