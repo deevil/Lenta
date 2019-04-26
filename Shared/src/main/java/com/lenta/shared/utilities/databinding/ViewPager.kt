c27a945dee5ab8f96c1be626053e78a0d81ed4ce
Denis Borisnko <borisenkoda@gmail.com>
2019-04-26 18:18:24 +0300

    DataBinding adapter for ViewPager

---
+++ Shared/src/main/java/com/lenta/shared/utilities/databinding/ViewPager.kt
@@ -0,0 +1,90 @@
+package com.lenta.shared.utilities.databinding
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.BindingAdapter
+import androidx.viewpager.widget.PagerAdapter
+import androidx.viewpager.widget.ViewPager
+import com.lenta.shared.utilities.extentions.implementationOf
+
+
+
+@BindingAdapter(value = ["viewPagerSettings", "position", "pageSelectionListener"], requireAll = false)
+fun setupViewPager(viewPager: ViewPager,
+                   viewPagerSettings: ViewPagerSettings?,
+                   position: Int?,
+                   pageSelectionListener: PageSelectionListener?) {
+
+    if (viewPagerSettings == null) {
+        return
+    }
+
+    if (viewPager.adapter == null) {
+        viewPager.let {
+            it.adapter = ViewPagerAdapter(viewPagerSettings)
+            it.offscreenPageLimit = viewPagerSettings.countTab()
+            it.currentItem = position ?: 0
+        }
+    }
+
+    viewPager.tag = pageSelectionListener
+
+    pageSelectionListener?.let {
+        viewPager.addOnAdapterChangeListener(object : ViewPager.OnPageChangeListener, ViewPager.OnAdapterChangeListener {
+            override fun onAdapterChanged(viewPager: ViewPager, oldAdapter: PagerAdapter?, newAdapter: PagerAdapter?) {
+            }
+
+            override fun onPageScrollStateChanged(state: Int) {
+            }
+
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+            }
+
+            override fun onPageSelected(position: Int) {
+                viewPager
+                        .tag
+                        .implementationOf(PageSelectionListener::class.java)
+                        ?.onPageSelected(position)
+            }
+
+        })
+    }
+
+}
+
+interface ViewPagerSettings {
+
+    fun getPagerItemView(container: ViewGroup, position: Int): View
+
+    fun getTextTitle(position: Int): String
+
+    fun countTab(): Int
+
+}
+
+interface PageSelectionListener {
+    fun onPageSelected(position: Int)
+}
+
+internal class ViewPagerAdapter(val viewPagerSettings: ViewPagerSettings) : PagerAdapter() {
+
+    override fun getCount(): Int = viewPagerSettings.countTab()
+
+    override fun isViewFromObject(view: View, `object`: Any): Boolean {
+        return view === `object`
+    }
+
+    override fun instantiateItem(container: ViewGroup, position: Int): Any {
+        val view = viewPagerSettings.getPagerItemView(container, position)
+        container.addView(view)
+        return view
+    }
+
+    override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+        container.removeView(`object` as View)
+    }
+
+    override fun getPageTitle(position: Int): CharSequence {
+        return viewPagerSettings.getTextTitle(position)
+    }
+}
--- writeoff_breakage/src/main/java/com/lenta/bp10/features/goods_list/GoodsListFragment.kt
+++ writeoff_breakage/src/main/java/com/lenta/bp10/features/goods_list/GoodsListFragment.kt
@@ -1,5 +1,8 @@
 package com.lenta.bp10.features.goods_list
 
+import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
 import com.lenta.bp10.R
 import com.lenta.bp10.databinding.FragmentGoodsListBinding
 import com.lenta.bp10.platform.extentions.getAppComponent
@@ -7,9 +10,15 @@ import com.lenta.shared.platform.fragment.CoreFragment
 import com.lenta.shared.platform.toolbar.bottom_toolbar.BottomToolbarUiModel
 import com.lenta.shared.platform.toolbar.bottom_toolbar.ButtonDecorationInfo
 import com.lenta.shared.platform.toolbar.top_toolbar.TopToolbarUiModel
+import com.lenta.shared.utilities.Logg
+import com.lenta.shared.utilities.databinding.PageSelectionListener
+import com.lenta.shared.utilities.databinding.ViewPagerSettings
 import com.lenta.shared.utilities.extentions.provideViewModel
 
-class GoodsListFragment : CoreFragment<FragmentGoodsListBinding, GoodsListViewModel>() {
+class GoodsListFragment :
+        CoreFragment<FragmentGoodsListBinding, GoodsListViewModel>(),
+        ViewPagerSettings,
+        PageSelectionListener {
 
     override fun getLayoutId(): Int = R.layout.fragment_goods_list
 
@@ -32,6 +41,25 @@ class GoodsListFragment : CoreFragment<FragmentGoodsListBinding, GoodsListViewMo
         bottomToolbarUiModel.uiModelButton4.show(ButtonDecorationInfo.print, enabled = false)
         bottomToolbarUiModel.uiModelButton5.show(ButtonDecorationInfo.save, enabled = false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding?.let {
+            it.viewPagerSettings = this
+
+        }
+
+    }
+
+    override fun getPagerItemView(container: ViewGroup, position: Int): View = View(context)
+
+    override fun getTextTitle(position: Int): String = getString(if (position == 0) R.string.counted else R.string.filter)
+
+    override fun onPageSelected(position: Int) {
+        Logg.d { "onPageSelected $position" }
+    }
+
+    override fun countTab(): Int = 2
 
 
 }
