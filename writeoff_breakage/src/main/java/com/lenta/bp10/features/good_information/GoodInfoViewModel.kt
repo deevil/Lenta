7493b5e0d6ad03fdd6df8c79439867e61ca0c5b2
Deins Borisenko <denis.borisenko@krit.pro>
2019-05-06 11:52:05 +0300

    fragment_good_info layout

--- writeoff_breakage/src/main/java/com/lenta/bp10/features/good_information/GoodInfoViewModel.kt
+++ writeoff_breakage/src/main/java/com/lenta/bp10/features/good_information/GoodInfoViewModel.kt
@@ -2,15 +2,36 @@ package com.lenta.bp10.features.good_information
 
 import androidx.lifecycle.MutableLiveData
 import com.lenta.shared.platform.viewmodel.CoreViewModel
+import com.lenta.shared.view.OnPositionClickListener
 
-class GoodInfoViewModel : CoreViewModel() {
+class GoodInfoViewModel : CoreViewModel(), OnPositionClickListener {
     private lateinit var goodCode: String
+
+    val writeOffReasonTitles: MutableLiveData<List<String>> = MutableLiveData()
+
+    val selectedPosition: MutableLiveData<Int> = MutableLiveData()
+
+    val count: MutableLiveData<String> = MutableLiveData()
+
+    val totalCount: MutableLiveData<String> = MutableLiveData()
 
     fun setGoodCode(goodCode: String) {
         this.goodCode = goodCode
     }
 
     val goodTitle = MutableLiveData("")
+
+    init {
+        //TODO (DB) нужно удалить фейковые данные
+        writeOffReasonTitles.value = listOf("Лом бой", "Поврежд.целост.ткани", "Температурный режим", "Нарушен тврн. вида")
+        count.value = "1 шт"
+        totalCount.value = "1 шт"
+
+    }
+
+    override fun onClickPosition(position: Int) {
+
+    }
 
 
 }
