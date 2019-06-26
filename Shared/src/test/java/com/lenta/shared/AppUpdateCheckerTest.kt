4fd1c2a4d81da883864fb1b34e8f1f50e58d50ee
Denis Borisnko <borisenkoda@gmail.com>
2019-06-26 18:53:24 +0300

    mockkObject tests

--- Shared/src/test/java/com/lenta/shared/AppUpdateCheckerTest.kt
+++ Shared/src/test/java/com/lenta/shared/AppUpdateCheckerTest.kt
@@ -2,23 +2,30 @@ package com.lenta.shared
 
 import android.content.Context
 import com.lenta.shared.platform.app_update.AppUpdateChecker
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.mockkObject
 import junit.framework.TestCase.assertFalse
 import junit.framework.TestCase.assertTrue
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.junit.MockitoJUnitRunner
-import org.mockito.Mock
-
 
 @RunWith(MockitoJUnitRunner::class)
 class AppUpdateCheckerTest {
-    @Mock
+    @MockK
     lateinit var context: Context
 
+    @Before
+    fun setUp() = MockKAnnotations.init(this, relaxUnitFun = true)
 
     @Test
     fun appUpdateCheckerTest() {
+
         val appUpdateChecker = AppUpdateChecker(context)
+
         assertTrue(appUpdateChecker.isNeedUpdate("10.0.165", "10.0.164"))
         assertTrue(appUpdateChecker.isNeedUpdate("10.0.165", "9.99.999"))
         assertTrue(appUpdateChecker.isNeedUpdate("10.0.165", "9.999.999"))
@@ -28,6 +35,16 @@ class AppUpdateCheckerTest {
         assertFalse(appUpdateChecker.isNeedUpdate("10.0.165", "10.0.166"))
         assertFalse(appUpdateChecker.isNeedUpdate("10.0.165", "11.0.0"))
         assertFalse(appUpdateChecker.isNeedUpdate("10.0.165", "10.1.0"))
+
+
+        mockkObject(appUpdateChecker)
+        every {appUpdateChecker.getCurrentVersionAppName()} returns "10.0.164"
+
+        assertTrue(appUpdateChecker.isNeedUpdate("10.7.0.1"))
+        assertTrue(appUpdateChecker.isNeedUpdate("10.0.165"))
+
+        assertFalse(appUpdateChecker.isNeedUpdate("10.0.164"))
+
     }
 
 
