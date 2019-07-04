ee51aa7639fe288d377ea1c3c152bf1ba280defa
dendima <dendima80@gmail.com>
2019-07-04 15:45:59 +0300

    inventory models refactoring getTaskSaveModel

--- inventory/src/main/java/com/lenta/inventory/models/task/InventoryTask.kt
+++ inventory/src/main/java/com/lenta/inventory/models/task/InventoryTask.kt
@@ -4,9 +4,9 @@ import com.lenta.inventory.models.repositories.ITaskRepository
 
 class InventoryTask(val taskDescription: TaskDescription, val taskRepository: ITaskRepository) {
 
-    //TODO возвращает модель сохранения задания
+    //возвращает модель сохранения задания
     fun getTaskSaveModel(): TaskSaveModel {
-        return TaskSaveModel(taskDescription!!)
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     //вызывается при возврате на 20 экран и при нажатии на кнопку ОБНОВИТЬ, вызываем 96 рест
