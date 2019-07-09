074cc97e8be331ca5dc7f5854bd3c91b0ac92a6c
Denis Borisnko <borisenkoda@gmail.com>
2019-07-09 12:08:47 +0300

    confirmation for open blocked task

--- inventory/src/main/java/com/lenta/inventory/platform/navigation/ScreenNavigator.kt
+++ inventory/src/main/java/com/lenta/inventory/platform/navigation/ScreenNavigator.kt
@@ -1,6 +1,7 @@
 package com.lenta.inventory.platform.navigation
 
 import android.content.Context
+import com.lenta.inventory.R
 import com.lenta.inventory.features.main_menu.MainMenuFragment
 import com.lenta.inventory.features.auth.AuthFragment
 import com.lenta.inventory.features.discrepancies_found.DiscrepanciesFoundFragment
@@ -21,9 +22,11 @@ import com.lenta.inventory.features.storages_list.StoragesListFragment
 import com.lenta.inventory.features.task_list.TaskListFragment
 import com.lenta.inventory.models.task.TaskProductInfo
 import com.lenta.shared.account.IAuthenticator
+import com.lenta.shared.features.alert.AlertFragment
 import com.lenta.shared.platform.activity.ForegroundActivityProvider
 import com.lenta.shared.platform.navigation.ICoreNavigator
 import com.lenta.shared.platform.navigation.runOrPostpone
+import com.lenta.shared.platform.toolbar.bottom_toolbar.ButtonDecorationInfo
 import com.lenta.shared.progress.IProgressUseCaseInformator
 
 class ScreenNavigator(
@@ -154,6 +157,13 @@ class ScreenNavigator(
         }
     }
 
+    override fun openConfirmationTaskOpenScreen(userName: String, ip: String, codeConfirmation: Int) {
+        runOrPostpone {
+            getFragmentStack()?.push(AlertFragment.create(message = context.getString(R.string.confirmation_task_open, userName, ip),
+                    codeConfirm = codeConfirmation, pageNumber = "93", rightButtonDecorationInfo = ButtonDecorationInfo.next))
+        }
+    }
+
     private fun getFragmentStack() = foregroundActivityProvider.getActivity()?.fragmentStack
 
 }
@@ -178,4 +188,5 @@ interface IScreenNavigator : ICoreNavigator {
     fun openJobCard(taskNumber: String)
     fun openLoadingTasksScreen()
     fun openExciseAlcoInfoScreen()
+    fun openConfirmationTaskOpenScreen(userName: String, ip: String, codeConfirmation: Int)
 }
