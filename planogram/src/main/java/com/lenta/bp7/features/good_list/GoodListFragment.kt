510e89275244911b1ad5ba8a41490c4571ff4221
Maksim Kulikov <maxim.kulikov@osinit.com>
2019-07-22 13:27:55 +0500

    Вернул открытие карточки уже созданного товара

--- planogram/src/main/java/com/lenta/bp7/features/good_list/GoodListFragment.kt
+++ planogram/src/main/java/com/lenta/bp7/features/good_list/GoodListFragment.kt
@@ -26,6 +26,8 @@ import com.lenta.shared.utilities.extentions.provideViewModel
 class GoodListFragment : CoreFragment<FragmentGoodListBinding, GoodListViewModel>(),
         ToolbarButtonsClickListener, OnBackPresserListener {
 
+    private var recyclerViewKeyHandler: RecyclerViewKeyHandler<*>? = null
+
     override fun getLayoutId(): Int = R.layout.fragment_good_list
 
     override fun getPageNumber(): String? = generateScreenNumberFromPostfix("12")
@@ -70,9 +72,38 @@ class GoodListFragment : CoreFragment<FragmentGoodListBinding, GoodListViewModel
 
     private fun initRvConfig() {
         binding?.let { layoutBinding ->
-            layoutBinding.rvConfig = DataBindingRecyclerViewConfig<ItemGoodBinding>(
+            layoutBinding.rvConfig = DataBindingRecyclerViewConfig(
                     layoutId = R.layout.item_good,
-                    itemId = BR.good
+                    itemId = BR.good,
+                    realisation = object : DataBindingAdapter<ItemGoodBinding> {
+                        override fun onCreate(binding: ItemGoodBinding) {
+                        }
+
+                        override fun onBind(binding: ItemGoodBinding, position: Int) {
+                            recyclerViewKeyHandler?.let {
+                                binding.root.isSelected = it.isSelected(position)
+                            }
+                        }
+                    },
+                    onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
+                        recyclerViewKeyHandler?.let {
+                            if (it.isSelected(position)) {
+                                vm.onClickItemPosition(position)
+                            } else {
+                                it.selectPosition(position)
+                            }
+                        }
+
+                    }
+            )
+
+            layoutBinding.vm = vm
+            layoutBinding.lifecycleOwner = viewLifecycleOwner
+            recyclerViewKeyHandler = RecyclerViewKeyHandler(
+                    rv = layoutBinding.rv,
+                    items = vm.goods,
+                    lifecycleOwner = layoutBinding.lifecycleOwner!!,
+                    initPosInfo = recyclerViewKeyHandler?.posInfo?.value
             )
         }
     }
