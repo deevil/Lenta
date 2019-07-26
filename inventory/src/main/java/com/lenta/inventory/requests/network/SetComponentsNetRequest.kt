d284c54f4abad588991e08d31e79555fa4747db1
dendima <dendima80@gmail.com>
2019-07-26 13:05:45 +0300

    INV Sets refact

--- inventory/src/main/java/com/lenta/inventory/requests/network/SetComponentsNetRequest.kt
+++ inventory/src/main/java/com/lenta/inventory/requests/network/SetComponentsNetRequest.kt
@@ -1,36 +1,35 @@
 package com.lenta.inventory.requests.network
 
-import com.google.gson.Gson
 import com.google.gson.annotations.SerializedName
-import com.lenta.shared.account.ISessionInfo
 import com.lenta.shared.exception.Failure
 import com.lenta.shared.fmp.ObjectRawStatus
-import com.lenta.shared.fmp.toFmpObjectRawStatusEither
 import com.lenta.shared.functional.Either
 import com.lenta.shared.interactor.UseCase
-import com.lenta.shared.utilities.Logg
-import com.mobrun.plugin.api.HyperHive
-import com.mobrun.plugin.api.callparams.WebCallParams
+import com.lenta.shared.requests.FmpRequestsHelper
 import javax.inject.Inject
 
 class SetComponentsNetRequest
-@Inject constructor(private val hyperHive: HyperHive, private val gson: Gson, private val sessionInfo: ISessionInfo) : UseCase<List<SetComponentsRestInfo>, Nothing?>(){
+@Inject constructor(private val fmpRequestsHelper: FmpRequestsHelper) : UseCase<List<SetComponentsRestInfo>, Nothing?>(){
     override suspend fun run(params: Nothing?): Either<Failure, List<SetComponentsRestInfo>> {
 
-        val webCallParams = WebCallParams().apply {
+        /**val webCallParams = WebCallParams().apply {
             headers = mapOf(
                     "X-SUP-DOMAIN" to "DM-MAIN",
                     "Content-Type" to "application/json",
                     "Web-Authorization" to sessionInfo.basicAuth
             )
-        }
+        }*/
 
         /**val resString = hyperHive.requestAPI.web("ZMP_UTZ_46_V001", webCallParams). execute()
         Logg.d { "resString: $resString" }*/
 
-        val res = hyperHive.requestAPI.web("ZMP_UTZ_46_V001", webCallParams).execute().toFmpObjectRawStatusEither(SetComponentsStatus::class.java, gson)
+        //val res = hyperHive.requestAPI.web("ZMP_UTZ_46_V001", webCallParams).execute().toFmpObjectRawStatusEither(SetComponentsStatus::class.java, gson)
 
-        return res
+        return fmpRequestsHelper.restRequest(
+                resourceName = "ZMP_UTZ_46_V001",
+                data = null,
+                clazz = SetComponentsStatus::class.java
+        )
     }
 }
 
