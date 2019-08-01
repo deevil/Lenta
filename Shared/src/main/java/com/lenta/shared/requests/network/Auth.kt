ff831684ec6dad00c3b249657922ac4146069fc4
Denis Borisnko <borisenkoda@gmail.com>
2019-08-01 14:22:46 +0300

    url encode login and password

--- Shared/src/main/java/com/lenta/shared/requests/network/Auth.kt
+++ Shared/src/main/java/com/lenta/shared/requests/network/Auth.kt
@@ -8,13 +8,14 @@ import com.lenta.shared.utilities.extentions.hhive.ANALYTICS_HELPER
 import com.lenta.shared.utilities.extentions.hhive.toEitherBoolean
 import com.lenta.shared.utilities.runIfRelease
 import com.mobrun.plugin.api.HyperHive
+import java.net.URLEncoder
 import javax.inject.Inject
 
 class Auth
 @Inject constructor(private val hyperHive: HyperHive, private val analytics: IAnalytics) : UseCase<Boolean, AuthParams>() {
     override suspend fun run(params: AuthParams): Either<Failure, Boolean> {
         ANALYTICS_HELPER?.onStartFmpRequest("AUTH")
-        return hyperHive.authAPI.auth(params.login, params.password, true).execute().toEitherBoolean("AUTH").apply {
+        return hyperHive.authAPI.auth(URLEncoder.encode(params.login, "UTF-8"), URLEncoder.encode(params.password, "UTF-8"), true).execute().toEitherBoolean("AUTH").apply {
             if (this.isRight) {
                 runIfRelease {
                     analytics.init()
