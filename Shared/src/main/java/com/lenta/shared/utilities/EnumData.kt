029e6ed20478c557f3b9758655d23c1545538f28
Maksim Kulikov <maxim.kulikov@osinit.com>
2019-08-01 19:27:55 +0500

    Предупреждение при смене тех. логина, если есть несохраненные данные

---
+++ Shared/src/main/java/com/lenta/shared/utilities/EnumData.kt
@@ -0,0 +1,9 @@
+package com.lenta.shared.utilities
+
+
+enum class PackageName(val path: String) {
+    PLE("com.lenta.bp7"),
+    WOB("com.lenta.bp10"),
+    INV("com.lenta.inventory"),
+    SHA("com.lenta.shared")
+}
