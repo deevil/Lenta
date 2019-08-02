fbbc72568966d929ab13c7db7bb5e77a3de2cb61
Maksim Kulikov <maxim.kulikov@osinit.com>
2019-08-02 17:15:42 +0500

    Подключены JUnit 5 тесты

---
+++ planogram/src/test/java/com/lenta/bp7/data/model/CheckDataTest.kt
@@ -0,0 +1,178 @@
+package com.lenta.bp7.data.model
+
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+internal class CheckDataTest {
+
+    @BeforeEach
+    fun setUp() {
+    }
+
+    @AfterEach
+    fun tearDown() {
+    }
+
+    @Test
+    fun testTest() {
+        assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun getSegments() {
+    }
+
+    @Test
+    fun getMarketNumber() {
+    }
+
+    @Test
+    fun setMarketNumber() {
+    }
+
+    @Test
+    fun getCheckType() {
+    }
+
+    @Test
+    fun setCheckType() {
+    }
+
+    @Test
+    fun getCountFacings() {
+    }
+
+    @Test
+    fun setCountFacings() {
+    }
+
+    @Test
+    fun getCheckEmptyPlaces() {
+    }
+
+    @Test
+    fun setCheckEmptyPlaces() {
+    }
+
+    @Test
+    fun getCurrentSegmentIndex() {
+    }
+
+    @Test
+    fun setCurrentSegmentIndex() {
+    }
+
+    @Test
+    fun getCurrentShelfIndex() {
+    }
+
+    @Test
+    fun setCurrentShelfIndex() {
+    }
+
+    @Test
+    fun getCurrentGoodIndex() {
+    }
+
+    @Test
+    fun setCurrentGoodIndex() {
+    }
+
+    @Test
+    fun isExistUnsentData() {
+    }
+
+    @Test
+    fun saveCheckResult() {
+    }
+
+    @Test
+    fun clearSavedData() {
+    }
+
+    @Test
+    fun getCurrentSegment() {
+    }
+
+    @Test
+    fun getCurrentShelf() {
+    }
+
+    @Test
+    fun getCurrentGood() {
+    }
+
+    @Test
+    fun getPreviousGood() {
+    }
+
+    @Test
+    fun addSegment() {
+    }
+
+    @Test
+    fun addShelf() {
+    }
+
+    @Test
+    fun addGood() {
+    }
+
+    @Test
+    fun deleteCurrentSegment() {
+    }
+
+    @Test
+    fun deleteCurrentShelf() {
+    }
+
+    @Test
+    fun deleteCurrentGood() {
+    }
+
+    @Test
+    fun removeAllFinishedSegments() {
+    }
+
+    @Test
+    fun setCurrentSegmentStatus() {
+    }
+
+    @Test
+    fun setCurrentShelfStatus() {
+    }
+
+    @Test
+    fun setCurrentGoodStatus() {
+    }
+
+    @Test
+    fun setShelfStatusDeletedByIndex() {
+    }
+
+    @Test
+    fun isExistUnfinishedSegment() {
+    }
+
+    @Test
+    fun setUnfinishedSegmentAsCurrent() {
+    }
+
+    @Test
+    fun getPreviousGoodFacings() {
+    }
+
+    @Test
+    fun getFormattedMarketNumber() {
+    }
+
+    @Test
+    fun restoreSavedCheckResult() {
+    }
+
+    @Test
+    fun prepareXmlCheckResult() {
+    }
+}
