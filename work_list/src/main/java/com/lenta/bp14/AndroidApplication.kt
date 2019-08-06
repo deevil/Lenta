94b2c27b1ae33c6a2821937c3c3fddec6885050a
Denis Borisnko <borisenkoda@gmail.com>
2019-08-06 11:52:18 +0300

    init bp14 - work list (wkl)

---
+++ work_list/src/main/java/com/lenta/bp14/AndroidApplication.kt
@@ -0,0 +1,35 @@
+package com.lenta.bp14
+
+import com.lenta.bp14.platform.extentions.getAppComponent
+import com.lenta.shared.CoreApplication
+import com.lenta.shared.settings.DefaultConnectionSettings
+
+class AndroidApplication : CoreApplication() {
+    override fun getDefaultConnectionSettings(): DefaultConnectionSettings {
+        return DefaultConnectionSettings(
+                serverAddress = "http://9.6.24.46",
+                environment = "Lenta_LRQ",
+                project = "PR_SHARED",
+                testServerAddress = "http://9.6.24.46",
+                testEnvironment = "Lenta_LRQ",
+                testProject = "PR_SHARED",
+                techLogin = "tech_user",
+                techPassword = "123456"
+        )
+    }
+
+    override fun onHandleException() {
+        ExceptionHandler().let {
+            getAppComponent(coreComponent).inject(it)
+            it.handleException()
+        }
+
+    }
+}
+
+class ExceptionHandler {
+
+    fun handleException() {
+        //TODO need implement
+    }
+}
---
+++ work_list/src/main/java/com/lenta/bp14/features/auth/AuthFragment.kt
@@ -0,0 +1,40 @@
+package com.lenta.bp14.features.auth
+
+import android.os.Bundle
+import android.view.View
+import com.lenta.bp14.R
+import com.lenta.bp14.platform.extentions.getAppComponent
+import com.lenta.bp14.platform.extentions.getAppTitle
+import com.lenta.shared.features.login.CoreAuthViewModel
+import com.lenta.shared.features.login.CoreLoginFragment
+import com.lenta.shared.platform.toolbar.top_toolbar.ImageButtonDecorationInfo
+import com.lenta.shared.platform.toolbar.top_toolbar.TopToolbarUiModel
+import com.lenta.shared.utilities.extentions.provideViewModel
+
+class AuthFragment : CoreLoginFragment() {
+
+    override fun getPageNumber(): String = "9/01"
+
+    override fun getViewModel(): CoreAuthViewModel {
+        provideViewModel(AuthViewModel::class.java).let {
+            getAppComponent()?.inject(it)
+            return it
+        }
+    }
+
+    override fun setupTopToolBar(topToolbarUiModel: TopToolbarUiModel) {
+        topToolbarUiModel.title.value = getAppTitle()
+        topToolbarUiModel.description.value = getString(R.string.authorization)
+        topToolbarUiModel.uiModelButton1.show(ImageButtonDecorationInfo.settings)
+        topToolbarUiModel.uiModelButton2.show(ImageButtonDecorationInfo.exitFromApp)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        vm.appTitle.value = getAppTitle()
+    }
+
+
+
+
+}
