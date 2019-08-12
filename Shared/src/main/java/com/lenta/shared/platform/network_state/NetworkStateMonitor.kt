3311f249917d2dddc32d71ce0e4e87d6557015b2
Denis Borisnko <borisenkoda@gmail.com>
2019-08-12 17:13:01 +0300

    Fix network state detection

--- Shared/src/main/java/com/lenta/shared/platform/network_state/NetworkStateMonitor.kt
+++ Shared/src/main/java/com/lenta/shared/platform/network_state/NetworkStateMonitor.kt
@@ -10,14 +10,22 @@ import androidx.lifecycle.MutableLiveData
 import com.lenta.shared.utilities.Logg
 import com.lenta.shared.utilities.extentions.getDeviceIp
 
+
 class NetworkStateMonitor : BroadcastReceiver(), INetworkStateMonitor {
 
     override val networkInfo: MutableLiveData<NetworkInfo> = MutableLiveData(NetworkInfo.noInternet)
 
     override fun onReceive(context: Context, intent: Intent) {
-        val noConnectivity = intent.getBooleanExtra(ConnectivityManager.EXTRA_NO_CONNECTIVITY, false)
-        networkInfo.postValue(if (noConnectivity) NetworkInfo.noInternet else NetworkInfo.connected(context.getDeviceIp()))
-        Logg.d { "Internet connected: ${!noConnectivity}" }
+
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager?
+        val activeNetworkInfo = connectivityManager?.activeNetworkInfo
+        val isConnected = activeNetworkInfo?.isConnected == true
+
+        val deviceIp = context.getDeviceIp()
+        networkInfo.postValue(if (!isConnected) NetworkInfo.noInternet else NetworkInfo.connected(deviceIp))
+        Logg.d { "Internet connected: $isConnected" }
+
+
     }
 
     fun start(activity: Activity) {
