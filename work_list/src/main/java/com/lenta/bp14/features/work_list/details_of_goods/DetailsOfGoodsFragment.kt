9afde9e92146af50f49c360181fdbb8f4038492e
Denis Borisnko <borisenkoda@gmail.com>
2019-08-15 17:20:07 +0300

    wkl - expiration tabs for details of goods fragment

--- work_list/src/main/java/com/lenta/bp14/features/work_list/details_of_goods/DetailsOfGoodsFragment.kt
+++ work_list/src/main/java/com/lenta/bp14/features/work_list/details_of_goods/DetailsOfGoodsFragment.kt
@@ -1,16 +1,23 @@
 package com.lenta.bp14.features.work_list.details_of_goods
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.lenta.bp14.BR
 import com.lenta.bp14.R
 import com.lenta.bp14.databinding.FragmentDetailsOfGoodsBinding
+import com.lenta.bp14.databinding.ItemTileExpirationBinding
+import com.lenta.bp14.databinding.LayoutExpirationDatesListBinding
+import com.lenta.bp14.databinding.LayoutTaskListBinding
 import com.lenta.bp14.platform.extentions.getAppComponent
 import com.lenta.shared.platform.fragment.CoreFragment
 import com.lenta.shared.platform.toolbar.bottom_toolbar.BottomToolbarUiModel
 import com.lenta.shared.platform.toolbar.bottom_toolbar.ButtonDecorationInfo
 import com.lenta.shared.platform.toolbar.bottom_toolbar.ToolbarButtonsClickListener
 import com.lenta.shared.platform.toolbar.top_toolbar.TopToolbarUiModel
+import com.lenta.shared.utilities.databinding.DataBindingRecyclerViewConfig
 import com.lenta.shared.utilities.databinding.ViewPagerSettings
 import com.lenta.shared.utilities.extentions.connectLiveData
 import com.lenta.shared.utilities.extentions.provideViewModel
@@ -50,7 +57,26 @@ class DetailsOfGoodsFragment : CoreFragment<FragmentDetailsOfGoodsBinding, Detai
     }
 
     override fun getPagerItemView(container: ViewGroup, position: Int): View {
+        if (position == 0) {
+            DataBindingUtil
+                    .inflate<LayoutExpirationDatesListBinding>(LayoutInflater.from(container.context),
+                            R.layout.layout_expiration_dates_list,
+                            container,
+                            false).let { layoutBinding ->
+
+                        layoutBinding.rvConfig = DataBindingRecyclerViewConfig<ItemTileExpirationBinding>(
+                                layoutId = R.layout.item_tile_expiration,
+                                itemId = BR.vm
+                        )
+
+                        layoutBinding.vm = vm
+                        layoutBinding.lifecycleOwner = viewLifecycleOwner
+                        return layoutBinding.root
+                    }
+        }
+
         return View(context)
+
     }
 
     override fun getTextTitle(position: Int): String {
