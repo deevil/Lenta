133b9a3292eed0d0f35923079704344a28f84d33
Maksim Kulikov <maxim.kulikov@osinit.com>
2019-08-16 18:09:30 +0500

    Инфо о товаре - немного логики

--- work_list/src/main/java/com/lenta/bp14/data/model/Good.kt
+++ work_list/src/main/java/com/lenta/bp14/data/model/Good.kt
@@ -3,7 +3,8 @@ package com.lenta.bp14.data.model
 
 data class Good(
         val material: String?,
-        val name: String
+        val name: String,
+        val total: Int = 0
 ) {
 
     fun getFormattedMaterial(): String? {
--- work_list/src/main/java/com/lenta/bp14/features/good_info_work_list/GoodInfoWorkListViewModel.kt
+++ work_list/src/main/java/com/lenta/bp14/features/good_info_work_list/GoodInfoWorkListViewModel.kt
@@ -1,21 +1,36 @@
 package com.lenta.bp14.features.good_info_work_list
 
-import com.lenta.shared.platform.viewmodel.CoreViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.lenta.bp14.data.model.Good
+import com.lenta.shared.platform.viewmodel.CoreViewModel
 import com.lenta.shared.utilities.databinding.PageSelectionListener
+import com.lenta.shared.utilities.extentions.map
 import kotlinx.coroutines.launch
 
 class GoodInfoWorkListViewModel : CoreViewModel(), PageSelectionListener {
 
     val good: MutableLiveData<Good> = MutableLiveData()
 
+    val quantity: MutableLiveData<String> = MutableLiveData()
+
+    val totalQuantity: MutableLiveData<Int> = quantity.map {
+        val currentQuantity = if (it?.isNotEmpty() == true) it.toInt() else 0
+        val goodQuantity = if(good.value != null) good.value!!.total else 0
+        currentQuantity + goodQuantity
+    }
+
     val selectedPage = MutableLiveData(0)
 
     init {
         viewModelScope.launch {
-            good.value = Good("000000000000254128", "Кукуруза")
+            good.value = Good(
+                    material = "000000000000254128",
+                    name = "Кукуруза",
+                    total = 5
+            )
+
+            quantity.value = "1"
         }
     }
 
