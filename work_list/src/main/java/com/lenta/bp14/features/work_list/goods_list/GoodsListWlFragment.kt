f1e3fb9558f86bae5c8c2b65f463d2cdca5ffbc1
Maksim Kulikov <maxim.kulikov@osinit.com>
2019-08-19 18:18:38 +0500

    Небольшой рефакторинг

--- work_list/src/main/java/com/lenta/bp14/features/work_list/goods_list/GoodsListWlFragment.kt
+++ work_list/src/main/java/com/lenta/bp14/features/work_list/goods_list/GoodsListWlFragment.kt
@@ -8,9 +8,7 @@ import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import com.lenta.bp14.BR
 import com.lenta.bp14.R
-import com.lenta.bp14.databinding.FragmentGoodsListWlBinding
-import com.lenta.bp14.databinding.ItemGoodBinding
-import com.lenta.bp14.databinding.LayoutGoodsListWlBinding
+import com.lenta.bp14.databinding.*
 import com.lenta.bp14.platform.extentions.getAppComponent
 import com.lenta.shared.platform.fragment.CoreFragment
 import com.lenta.shared.platform.toolbar.bottom_toolbar.BottomToolbarUiModel
@@ -50,13 +48,13 @@ class GoodsListWlFragment : CoreFragment<FragmentGoodsListWlBinding, GoodsListWl
     }
 
     override fun getPagerItemView(container: ViewGroup, position: Int): View {
-            DataBindingUtil.inflate<LayoutGoodsListWlBinding>(LayoutInflater.from(container.context),
-                    R.layout.layout_goods_list_wl,
+            DataBindingUtil.inflate<LayoutWlGoodsListBinding>(LayoutInflater.from(container.context),
+                    R.layout.layout_wl_goods_list,
                     container,
                     false).let { layoutBinding ->
 
                 if (position == 0) {
-                    layoutBinding.rvConfig = DataBindingRecyclerViewConfig<ItemGoodBinding>(
+                    layoutBinding.rvConfig = DataBindingRecyclerViewConfig<ItemGoodSelectableBinding>(
                             layoutId = R.layout.item_good_selectable,
                             itemId = BR.good)
                 } else {
