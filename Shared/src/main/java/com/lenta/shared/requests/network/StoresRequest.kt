70819aefd2a13fb2924029332fea0f88da0e9356
Denis Borisnko <borisenkoda@gmail.com>
2019-08-20 17:01:58 +0300

    ple - first release (server 9.6.24.61:6444)

--- Shared/src/main/java/com/lenta/shared/requests/network/StoresRequest.kt
+++ Shared/src/main/java/com/lenta/shared/requests/network/StoresRequest.kt
@@ -3,17 +3,33 @@ package com.lenta.shared.requests.network
 import com.google.gson.annotations.SerializedName
 import com.lenta.shared.exception.Failure
 import com.lenta.shared.fmp.ObjectRawStatus
+import com.lenta.shared.fmp.resources.dao_ext.getAllMarkets
+import com.lenta.shared.fmp.resources.fast.ZmpUtz23V001
 import com.lenta.shared.functional.Either
 import com.lenta.shared.interactor.UseCase
 import com.lenta.shared.requests.FmpRequestsHelper
 import com.lenta.shared.requests.SapResponse
+import com.mobrun.plugin.api.HyperHive
 import javax.inject.Inject
 
 
 class StoresRequest
-@Inject constructor(private val fmpRequestsHelper: FmpRequestsHelper) : UseCase<StoresRequestResult, Nothing?>() {
+@Inject constructor(private val fmpRequestsHelper: FmpRequestsHelper, hyperHive: HyperHive) : UseCase<StoresRequestResult, Nothing?>() {
+
+    var zmpUtz23V001: ZmpUtz23V001 = ZmpUtz23V001(hyperHive)
+
     override suspend fun run(params: Nothing?): Either<Failure, StoresRequestResult> {
-        return fmpRequestsHelper.restRequest("ZMP_UTZ_WOB_01_V001", params, StoresRequestStatus::class.java)
+        return Either.Right(StoresRequestResult(
+                markets = zmpUtz23V001.getAllMarkets().map {
+                    Market(
+                            number = it.werks,
+                            address = it.addres,
+                            retailType = it.retailType
+                    )
+                },
+                errorText = "",
+                retCode = 0
+        ))
     }
 }
 
