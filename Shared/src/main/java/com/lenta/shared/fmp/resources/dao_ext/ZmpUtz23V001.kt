73d81121d29636600cf67f68da5a999ccb2f1003
Denis Borisnko <borisenkoda@gmail.com>
2019-08-20 16:48:45 +0300

    ple - first release

--- Shared/src/main/java/com/lenta/shared/fmp/resources/dao_ext/ZmpUtz23V001.kt
+++ Shared/src/main/java/com/lenta/shared/fmp/resources/dao_ext/ZmpUtz23V001.kt
@@ -8,3 +8,8 @@ fun ZmpUtz23V001.getRetailType(storeNumber: String): String? {
             .map { it.retailType }
             .firstOrNull()
 }
+
+fun ZmpUtz23V001.getAllMarkets(): List<ZmpUtz23V001.ItemLocal_ET_WERKS_ADR> {
+    @Suppress("INACCESSIBLE_TYPE")
+    return localHelper_ET_WERKS_ADR.all
+}
