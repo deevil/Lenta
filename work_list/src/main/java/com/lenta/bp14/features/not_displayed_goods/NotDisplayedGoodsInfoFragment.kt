711355f45b0789a14762cb814cfdecd565c1fa5b
Denis Borisnko <borisenkoda@gmail.com>
2019-08-21 12:26:07 +0300

    wkl - not displayed goods info fragment tabs

--- work_list/src/main/java/com/lenta/bp14/features/not_displayed_goods/NotDisplayedGoodsInfoFragment.kt
+++ work_list/src/main/java/com/lenta/bp14/features/not_displayed_goods/NotDisplayedGoodsInfoFragment.kt
@@ -1,17 +1,21 @@
 package com.lenta.bp14.features.not_displayed_goods
 
 import com.lenta.bp14.R
-import com.lenta.bp14.databinding.FragmentNotDisplayedGoodsInfoBinding
 import com.lenta.bp14.platform.extentions.getAppComponent
 import com.lenta.shared.platform.fragment.CoreFragment
 import com.lenta.shared.platform.toolbar.bottom_toolbar.BottomToolbarUiModel
 import com.lenta.shared.platform.toolbar.top_toolbar.TopToolbarUiModel
 import com.lenta.shared.utilities.extentions.provideViewModel
 import android.os.Bundle
+import android.view.LayoutInflater
 import com.lenta.shared.utilities.databinding.ViewPagerSettings
 import android.view.ViewGroup
 import android.view.View
+import androidx.databinding.DataBindingUtil
+import com.lenta.bp14.BR
+import com.lenta.bp14.databinding.*
 import com.lenta.shared.platform.toolbar.bottom_toolbar.ButtonDecorationInfo
+import com.lenta.shared.utilities.databinding.DataBindingRecyclerViewConfig
 
 class NotDisplayedGoodsInfoFragment : CoreFragment<FragmentNotDisplayedGoodsInfoBinding, NotDisplayedGoodsInfoViewModel>(), ViewPagerSettings {
 
@@ -42,7 +46,31 @@ class NotDisplayedGoodsInfoFragment : CoreFragment<FragmentNotDisplayedGoodsInfo
     }
 
     override fun getPagerItemView(container: ViewGroup, position: Int): View {
-        return View(context)
+        return when (position) {
+            0 -> DataBindingUtil
+                    .inflate<LayoutNotDisplayedCommonInfoBinding>(LayoutInflater.from(container.context),
+                            R.layout.layout_not_displayed_common_info,
+                            container,
+                            false).apply {
+                        this.vm = vm
+                        this.lifecycleOwner = viewLifecycleOwner
+                    }.root
+            else -> DataBindingUtil
+                    .inflate<LayoutNotDisplayedGoodsBalancesBinding>(LayoutInflater.from(container.context),
+                            R.layout.layout_not_displayed_goods_balances,
+                            container,
+                            false).let { layoutBinding ->
+
+                        layoutBinding.rvConfig = DataBindingRecyclerViewConfig<ItemTileExpirationBinding>(
+                                layoutId = R.layout.item_tile_stock,
+                                itemId = BR.vm
+                        )
+
+                        layoutBinding.vm = vm
+                        layoutBinding.lifecycleOwner = viewLifecycleOwner
+                        return layoutBinding.root
+                    }
+        }
     }
 
     override fun getTextTitle(position: Int): String {
