22da9e639fcc3699a626e4f6b3b1f6fa0473d023
Denis Borisnko <borisenkoda@gmail.com>
2019-08-29 15:11:22 +0300

    inv - fix removing product with stamps (1578)

--- inventory/src/main/java/com/lenta/inventory/features/goods_list/GoodsListViewModel.kt
+++ inventory/src/main/java/com/lenta/inventory/features/goods_list/GoodsListViewModel.kt
@@ -162,8 +162,8 @@ class GoodsListViewModel : CoreViewModel(), OnOkInSoftKeyboardListener {
     fun onClickClean() {
         if (selectedPage.value == 0) {
             unprocessedSelectionHelper.selectedPositions.value?.forEach {
-                unprocessedGoods.value?.get(it)?.matnr?.let {
-                    taskManager.getInventoryTask()!!.deleteProduct(it)
+                unprocessedGoods.value?.get(it)?.matnr?.let { matNr ->
+                    taskManager.getInventoryTask()!!.deleteProduct(matNr)
                 }
             }
             unprocessedSelectionHelper.clearPositions()
@@ -175,7 +175,10 @@ class GoodsListViewModel : CoreViewModel(), OnOkInSoftKeyboardListener {
                             ?: "")
 
                     if (productInfo != null) {
-                        taskManager.getInventoryTask()!!.taskRepository.getProducts().changeProduct(productInfo.copy(factCount = 0.0, isPositionCalc = false))
+                        taskManager.getInventoryTask()!!.taskRepository.apply {
+                            getExciseStamps().deleteExciseStampsForProduct(productInfo)
+                            getProducts().changeProduct(productInfo.copy(factCount = 0.0, isPositionCalc = false))
+                        }
                     }
                 }
             }
