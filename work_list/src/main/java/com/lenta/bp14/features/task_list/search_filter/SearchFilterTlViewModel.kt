5883b7483615d02179860b8dbff157371318e18a
Maksim Kulikov <maxim.kulikov@osinit.com>
2019-08-31 17:01:48 +0500

    Заготовка для фильтра поиска заданий

---
+++ work_list/src/main/java/com/lenta/bp14/features/task_list/search_filter/SearchFilterTlViewModel.kt
@@ -0,0 +1,8 @@
+package com.lenta.bp14.features.task_list.search_filter
+
+import com.lenta.shared.platform.viewmodel.CoreViewModel
+
+class SearchFilterTlViewModel : CoreViewModel() {
+
+    // TODO: Implement the ViewModel
+}
