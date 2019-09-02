46a87ff863ee0c05124aee7f5f6bd0a718eac59f
Maksim Kulikov <maxim.kulikov@osinit.com>
2019-09-02 11:37:14 +0500

    Логика фильтра заданий

--- work_list/src/main/java/com/lenta/bp14/features/task_list/search_filter/SearchFilterTlViewModel.kt
+++ work_list/src/main/java/com/lenta/bp14/features/task_list/search_filter/SearchFilterTlViewModel.kt
@@ -6,10 +6,12 @@ import com.lenta.bp14.data.TaskManager
 import com.lenta.bp14.data.model.TaskType
 import com.lenta.bp14.platform.navigation.IScreenNavigator
 import com.lenta.shared.platform.viewmodel.CoreViewModel
+import com.lenta.shared.view.OnPositionClickListener
 import kotlinx.coroutines.launch
+import java.lang.IllegalArgumentException
 import javax.inject.Inject
 
-class SearchFilterTlViewModel : CoreViewModel() {
+class SearchFilterTlViewModel : CoreViewModel(), OnPositionClickListener {
 
     @Inject
     lateinit var navigator: IScreenNavigator
@@ -20,7 +22,6 @@ class SearchFilterTlViewModel : CoreViewModel() {
     val marketNumber = MutableLiveData<String>("")
 
     var taskType: TaskType = TaskType.WORK_LIST
-
     val goodField = MutableLiveData<String>("")
     val sectionField = MutableLiveData<String>("")
     val goodsGroupField = MutableLiveData<String>("")
@@ -36,5 +37,15 @@ class SearchFilterTlViewModel : CoreViewModel() {
     fun onClickFind() {
         taskManager.setCurrentTaskFilter(taskType, goodField.value, sectionField.value, goodsGroupField.value, publicationDateField.value)
         navigator.openTaskListScreen()
+    }
+
+    override fun onClickPosition(position: Int) {
+        taskType = when (position) {
+            0 -> TaskType.WORK_LIST
+            1 -> TaskType.PRICE_CHECK
+            2 -> TaskType.CHECK_LIST
+            3 -> TaskType.NOT_EXPOSED
+            else -> throw IllegalArgumentException("Wrong task type position!")
+        }
     }
 }
