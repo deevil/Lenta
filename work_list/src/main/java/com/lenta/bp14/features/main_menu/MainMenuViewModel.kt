97a5b318532a9cdc4cd7bfa2a1f50cf460ca8d18
Denis Borisnko <borisenkoda@gmail.com>
2019-09-19 14:27:05 +0300

    wkl - incognito mode icon in status bar

--- work_list/src/main/java/com/lenta/bp14/features/main_menu/MainMenuViewModel.kt
+++ work_list/src/main/java/com/lenta/bp14/features/main_menu/MainMenuViewModel.kt
@@ -1,12 +1,9 @@
 package com.lenta.bp14.features.main_menu
 
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.viewModelScope
 import com.lenta.bp14.platform.navigation.IScreenNavigator
 import com.lenta.shared.account.ISessionInfo
 import com.lenta.shared.platform.viewmodel.CoreViewModel
 import com.lenta.shared.utilities.extentions.map
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class MainMenuViewModel : CoreViewModel() {
@@ -17,19 +14,17 @@ class MainMenuViewModel : CoreViewModel() {
     lateinit var sessionInfo: ISessionInfo
 
 
-    private val authorizationSkipped = MutableLiveData<Boolean>()
-
-    val authorizationButtonVisibility = authorizationSkipped.map { it == true }
-
-    val createTaskButtonVisibility = authorizationSkipped.map { it == false }
-    val workWithTaskButtonVisibility = authorizationSkipped.map { it == false }
-    val checkListButtonVisibility = authorizationSkipped.map { it == true }
-
-    init {
-        viewModelScope.launch {
-            authorizationSkipped.value = sessionInfo.authorizationSkipped
-        }
+    private val authorizationSkipped by lazy {
+        sessionInfo.isAuthSkipped.map { it == true }
     }
+
+    val authorizationButtonVisibility by lazy { authorizationSkipped.map { it } }
+
+
+    val createTaskButtonVisibility by lazy { authorizationSkipped.map { it == false } }
+    val workWithTaskButtonVisibility by lazy { authorizationSkipped.map { it == false } }
+    val checkListButtonVisibility by lazy { authorizationSkipped.map { it == true } }
+
 
     fun onClickPrint() {
         // Тестирование запуска различных экранов
