469a166177a2a22ade5e794f4a1281235ceda344
Borisenko Denis <borisenkoda@gmail.com>
2019-10-01 11:53:46 +0300

    wkl - result for TaskListNetRequest

--- work_list/src/main/java/com/lenta/bp14/requests/tasks/TaskListNetRequest.kt
+++ work_list/src/main/java/com/lenta/bp14/requests/tasks/TaskListNetRequest.kt
@@ -1,6 +1,7 @@
 package com.lenta.bp14.requests.tasks
 
 import com.google.gson.annotations.SerializedName
+import com.lenta.bp14.requests.pojo.RetCode
 import com.lenta.shared.exception.Failure
 import com.lenta.shared.fmp.ObjectRawStatus
 import com.lenta.shared.functional.Either
@@ -13,7 +14,6 @@ class TaskListNetRequest
 
     override suspend fun run(params: TasksListParams): Either<Failure, TaskListInfo> {
         return fmpRequestsHelper.restRequest("ZMP_UTZ_WKL_02_V001", params, TaskListInfoStatus::class.java)
-
     }
 }
 
@@ -22,9 +22,39 @@ class TaskListInfoStatus : ObjectRawStatus<TaskListInfo>()
 
 
 data class TaskListInfo(
-        @SerializedName("TASK_NUM")
-        val taskNumber: String
+        @SerializedName("ET_TASK_LIST")
+        val taskList: List<TaskInfo>,
+        @SerializedName("ET_RETCODE")
+        val retCodeList: List<RetCode>
+)
 
+data class TaskInfo(
+        @SerializedName("TASK_NUM")
+        val taskNumber: String,
+        @SerializedName("TASK_TYPE")
+        val taskType: String,
+        @SerializedName("TEXT1")
+        val text1: String,
+        @SerializedName("TEXT2")
+        val text2: String,
+        @SerializedName("TEXT3")
+        val text3: String,
+        //Тип блокировки: 1 - своя, 2 - не своя
+        @SerializedName("BLOCK_TYPE")
+        val blockType: String,
+        @SerializedName("LOCK_USER")
+        val lockUser: String,
+        @SerializedName("NOT_FINISH")
+        val notFinished: String,
+        // Количество позиций в задании
+        @SerializedName("QNT_POS")
+        val quantityPositions: Int,
+        @SerializedName("DESCR")
+        val taskName: String,
+        @SerializedName("IS_STRICT")
+        val isStrict: String,
+        @SerializedName("COMMENT")
+        val comment: String
 )
 
 
