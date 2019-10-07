cdeca1cd2c8373227cecf1f020610b6a1f439cf2
Borisenko Denis <denis.borisenko@krit.pro>
2019-10-07 17:58:11 +0300

    wkl - icon descriptions from db

---
+++ Shared/src/main/java/com/lenta/shared/fmp/resources/dao_ext/ZmpUtz38V001.kt
@@ -0,0 +1,27 @@
+package com.lenta.shared.fmp.resources.dao_ext
+
+import com.lenta.shared.fmp.resources.fast.ZmpUtz38V001
+
+fun ZmpUtz38V001.getIconDescriptionByCode(code: String): String? {
+    @Suppress("INACCESSIBLE_TYPE")
+    return localHelper_ET_ICONS.getWhere("CODE = \"$code\" LIMIT 1").getOrNull(0)?.text
+}
+
+fun ZmpUtz38V001.getIconDescription(iconCode: IconCode): String? {
+    @Suppress("INACCESSIBLE_TYPE")
+    return localHelper_ET_ICONS.getWhere("CODE = \"${iconCode.code}\" LIMIT 1").getOrNull(0)?.text
+}
+
+enum class IconCode(val code: String) {
+    EAN("01"),
+    EXCISE_STAMP("02"),
+    BOX_SCAN("03"),
+    SECTION_NUMBER("04"),
+    MATRIX_TYPE("05"),
+    NOT_EXCISE_ALCO("06"),
+    EXCISE_ALCO("07"),
+    RETURN_POSSIBLE("08"),
+    USUALY_PRODUCT("09"),
+    TASK_WITH_ALCO("10")
+}
+
