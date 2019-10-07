22664df05ba3b222e96f317b1f4b4cabc07b2744
Maksim Kulikov <maxim.kulikov@osinit.com>
2019-10-07 10:56:23 +0500

    wkl - ЧЛ / CheckListComponent

--- work_list/src/main/java/com/lenta/bp14/di/CheckListComponent.kt
+++ work_list/src/main/java/com/lenta/bp14/di/CheckListComponent.kt
@@ -1,15 +1,28 @@
 package com.lenta.bp14.di
 
+import com.lenta.bp14.features.check_list.goods_list.GoodsListClViewModel
+import com.lenta.bp14.models.check_list.CheckListTask
 import com.lenta.bp14.models.check_list.CheckListTaskDescription
-import dagger.*
+import com.lenta.bp14.models.check_list.CheckListTaskManager
+import com.lenta.bp14.models.check_list.ICheckListTask
+import com.lenta.bp14.models.check_list.repo.CheckListRepo
+import com.lenta.bp14.models.check_list.repo.ICheckListRepo
+import dagger.Binds
+import dagger.Component
+import dagger.Module
+import dagger.Provides
 import javax.inject.Scope
 
 @CheckListScope
 @Component(modules = [CheckListModule::class], dependencies = [AppComponent::class])
 
 interface CheckListComponent {
-}
 
+    fun inject(it: GoodsListClViewModel)
+    fun inject(it: CheckListTaskManager)
+    fun getCheckListTask(): ICheckListTask
+
+}
 
 @Module(includes = [CheckListModule.Declarations::class])
 class CheckListModule(private val taskDescription: CheckListTaskDescription) {
@@ -17,6 +30,20 @@ class CheckListModule(private val taskDescription: CheckListTaskDescription) {
     @Module
     internal interface Declarations {
 
+        @Binds
+        @CheckPriceScope
+        fun bindCheckListTask(realisation: CheckListTask): ICheckListTask
+
+        @Binds
+        @CheckPriceScope
+        fun bindCheckListRepo(realisation: CheckListRepo): ICheckListRepo
+
+    }
+
+    @Provides
+    @CheckPriceScope
+    internal fun provideTaskDescription(): CheckListTaskDescription {
+        return taskDescription
     }
 
 }
