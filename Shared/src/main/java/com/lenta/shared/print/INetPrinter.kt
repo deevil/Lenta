be369c117ceedf4b7c9de6ba6f8e262c43ed880e
Borisenko Denis <denis.borisenko@krit.pro>
2019-10-11 18:36:14 +0300

    shared - print zebra Insert BOM (Byte Order Mark)

--- Shared/src/main/java/com/lenta/shared/print/INetPrinter.kt
+++ Shared/src/main/java/com/lenta/shared/print/INetPrinter.kt
@@ -12,7 +12,7 @@ abstract class INetPrinter {
 
     private val CALIBRATE = "! U1 setvar \"media.type\" \"label\"\r\n! U1 setvar \"media.sense_mode\" \"bar\"\r\n~JC^XA^JUS^XZ\r\n! U1 do \"device.reset\" \"\"\r\n"
 
-    suspend fun print(data: String) {
+    open suspend fun print(data: String) {
         withContext(Dispatchers.IO) {
             val bytes = convertStringToBytes(data)
             val socket = Socket(ip, port)
--- Shared/src/main/java/com/lenta/shared/print/ZebraNetPrinter.kt
+++ Shared/src/main/java/com/lenta/shared/print/ZebraNetPrinter.kt
@@ -9,5 +9,29 @@ class ZebraNetPrinter(override val ip: String) : INetPrinter() {
 
     override val port: Int = 6101
 
+    override suspend fun print(data: String) {
+
+        // Insert BOM (Byte Order Mark)
+
+        //TODO закончит
+
+        val byteArray = data.toByteArray()
+
+        val byte0 = 0xEF.toByte()
+        val byte1 = 0xBB.toByte()
+        val byte2 = 0xBF.toByte()
+
+        if (byteArray[0] != byte0 && byteArray[1] != byte1 && byteArray[2] != byte2) {
+            ByteArray(3 + byteArray.size).apply {
+                set(0, byte0)
+                set(1, byte1)
+                set(2, byte2)
+            }
+
+        }
+
+        super.print(data)
+    }
+
 
 }
