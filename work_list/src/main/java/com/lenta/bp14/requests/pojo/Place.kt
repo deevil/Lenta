3d5fbd71f2708b0c3ee10ce9d75b64455f6f0839
Maksim Kulikov <maxim.kulikov@osinit.com>
2019-10-13 21:14:37 +0500

    wkl - РС / Получение задания

--- work_list/src/main/java/com/lenta/bp14/requests/pojo/Place.kt
+++ work_list/src/main/java/com/lenta/bp14/requests/pojo/Place.kt
@@ -3,8 +3,10 @@ package com.lenta.bp14.requests.pojo
 import com.google.gson.annotations.SerializedName
 
 data class Place(
+        /** SAP-код товара */
         @SerializedName("MATNR")
         var matnr: String,
+        /** Код места хранения */
         @SerializedName("PLACE_CODE")
         var placeCode: String
 )
