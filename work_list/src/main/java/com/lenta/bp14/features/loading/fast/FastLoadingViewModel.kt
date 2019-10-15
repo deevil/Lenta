93bf41325c0f319423998b2f1f8dcf17e22478de
Borisenko Denis <borisenkoda@gmail.com>
2019-10-15 12:06:46 +0300

    WKL  (2042) stores request fix

--- work_list/src/main/java/com/lenta/bp14/features/loading/fast/FastLoadingViewModel.kt
+++ work_list/src/main/java/com/lenta/bp14/features/loading/fast/FastLoadingViewModel.kt
@@ -4,15 +4,12 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.lenta.bp14.models.general.IGeneralRepo
 import com.lenta.bp14.platform.navigation.IScreenNavigator
-import com.lenta.bp14.repos.IRepoInMemoryHolder
 import com.lenta.bp14.requests.FastResourcesMultiRequest
 import com.lenta.shared.exception.Failure
 import com.lenta.shared.exception.IFailureInterpreter
 import com.lenta.shared.features.loading.CoreLoadingViewModel
 import com.lenta.shared.platform.app_update.AppUpdateChecker
 import com.lenta.shared.requests.network.Auth
-import com.lenta.shared.requests.network.StoresRequest
-import com.lenta.shared.requests.network.StoresRequestResult
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -27,10 +24,6 @@ class FastLoadingViewModel : CoreLoadingViewModel() {
     @Inject
     lateinit var appUpdateChecker: AppUpdateChecker
     @Inject
-    lateinit var repoInMemoryHolder: IRepoInMemoryHolder
-    @Inject
-    lateinit var storesRequest: StoresRequest
-    @Inject
     lateinit var generalRepo: IGeneralRepo
     @Inject
     lateinit var auth: Auth
@@ -43,16 +36,11 @@ class FastLoadingViewModel : CoreLoadingViewModel() {
     init {
         viewModelScope.launch {
             progress.value = true
-            fastResourcesNetRequest(null).either(::handleFailure, ::loadStores)
+            fastResourcesNetRequest(null).either(::handleFailure, ::handleSuccess)
         }
     }
 
-    private fun loadStores(@Suppress("UNUSED_PARAMETER") b: Boolean) {
-        viewModelScope.launch {
-            progress.value = true
-            storesRequest(null).either(::handleFailure, ::handleSuccess)
-        }
-    }
+
 
     override fun handleFailure(failure: Failure) {
         progress.value = false
@@ -60,10 +48,8 @@ class FastLoadingViewModel : CoreLoadingViewModel() {
         navigator.openAlertScreen(failureInterpreter.getFailureDescription(failure).message)
     }
 
-    private fun handleSuccess(storesRequestResult: StoresRequestResult) {
+    private fun handleSuccess(notUsed: Boolean) {
         progress.value = true
-        repoInMemoryHolder.storesRequestResult = storesRequestResult
-
         //TODO добавить проверку необходимости обновления
 
         /*viewModelScope.launch {
