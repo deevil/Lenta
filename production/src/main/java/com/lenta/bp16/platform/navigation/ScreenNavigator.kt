20e095b26ffaf188e56d833afcf86d69dd22b837
Maksim Kulikov <maxim.kulikov@osinit.com>
2019-11-15 12:05:59 +0500

    pro - Информационные экраны

--- production/src/main/java/com/lenta/bp16/platform/navigation/ScreenNavigator.kt
+++ production/src/main/java/com/lenta/bp16/platform/navigation/ScreenNavigator.kt
@@ -1,14 +1,17 @@
 package com.lenta.bp16.platform.navigation
 
 import android.content.Context
+import com.lenta.bp16.R
 import com.lenta.bp16.features.auth.AuthFragment
 import com.lenta.bp16.features.loading.fast.FastDataLoadingFragment
 import com.lenta.bp16.features.main_menu.MainMenuFragment
 import com.lenta.bp16.features.select_market.SelectMarketFragment
 import com.lenta.shared.account.IAuthenticator
+import com.lenta.shared.features.alert.AlertFragment
 import com.lenta.shared.platform.activity.ForegroundActivityProvider
 import com.lenta.shared.platform.navigation.ICoreNavigator
 import com.lenta.shared.platform.navigation.runOrPostpone
+import com.lenta.shared.platform.toolbar.bottom_toolbar.ButtonDecorationInfo
 import com.lenta.shared.progress.IProgressUseCaseInformator
 import javax.inject.Inject
 
@@ -20,11 +23,27 @@ class ScreenNavigator @Inject constructor(
         private val progressUseCaseInformator: IProgressUseCaseInformator
 ) : IScreenNavigator, ICoreNavigator by coreNavigator {
 
+    private fun getFragmentStack() = foregroundActivityProvider.getActivity()?.fragmentStack
+
     override fun openFirstScreen() {
         if (authenticator.isAuthorized()) {
             openMainMenuScreen()
         } else {
             openLoginScreen()
+        }
+    }
+
+
+    // Основные экраны
+    override fun openLoginScreen() {
+        runOrPostpone {
+            getFragmentStack()?.push(AuthFragment())
+        }
+    }
+
+    override fun openFastDataLoadingScreen() {
+        runOrPostpone {
+            getFragmentStack()?.push(FastDataLoadingFragment())
         }
     }
 
@@ -40,46 +59,45 @@ class ScreenNavigator @Inject constructor(
         }
     }
 
-    override fun openFastDataLoadingScreen() {
-        runOrPostpone {
-            getFragmentStack()?.push(FastDataLoadingFragment())
-        }
-    }
-
-    override fun openLoginScreen() {
-        runOrPostpone {
-            getFragmentStack()?.push(AuthFragment())
-        }
-    }
-
-
-    private fun getFragmentStack() = foregroundActivityProvider.getActivity()?.fragmentStack
-
 
     // Информационные экраны
-    /*override fun showConfirmPriceTagsPrinting(priceTagNumber: Int, confirmCallback: () -> Unit) {
+    override fun showDefrostingPhaseIsCompleted(nextCallback: () -> Unit) {
         runOrPostpone {
-            getFragmentStack()?.push(AlertFragment.create(message = context.getString(R.string.confirm_price_tags_printing, priceTagNumber),
-                    pageNumber = "10",
-                    iconRes = R.drawable.ic_question_80dp,
-                    codeConfirmForRight = backFragmentResultHelper.setFuncForResult(confirmCallback),
-                    rightButtonDecorationInfo = ButtonDecorationInfo.confirm))
+            getFragmentStack()?.push(AlertFragment.create(
+                    pageNumber = "12",
+                    message = context.getString(R.string.defrosting_phase_is_completed),
+                    iconRes = R.drawable.is_warning_yellow_80dp,
+                    isVisibleLeftButton = false,
+                    codeConfirmForRight = backFragmentResultHelper.setFuncForResult(nextCallback),
+                    rightButtonDecorationInfo = ButtonDecorationInfo.next
+            ))
         }
     }
 
-    override fun showPrintPriceOffer(goodName: String, noCallback: () -> Unit, yesCallback: () -> Unit) {
+    override fun showConfirmNoSuchItemLeft(confirmCallback: () -> Unit) {
         runOrPostpone {
             getFragmentStack()?.push(AlertFragment.create(
-                    message = context.getString(R.string.print_price_tag_for_good, goodName),
-                    pageNumber = "43",
-                    iconRes = R.drawable.ic_question_80dp,
-                    codeConfirmForLeft = backFragmentResultHelper.setFuncForResult(noCallback),
-                    codeConfirmForRight = backFragmentResultHelper.setFuncForResult(yesCallback),
-                    leftButtonDecorationInfo = ButtonDecorationInfo.no,
-                    rightButtonDecorationInfo = ButtonDecorationInfo.yes
+                    pageNumber = "23",
+                    message = context.getString(R.string.confirm_that_there_is_no_such_item_left_in_eo),
+                    iconRes = R.drawable.is_warning_yellow_80dp,
+                    codeConfirmForRight = backFragmentResultHelper.setFuncForResult(confirmCallback),
+                    rightButtonDecorationInfo = ButtonDecorationInfo.confirm
             ))
         }
-    }*/
+    }
+
+    override fun showFixingPackagingPhaseSuccessful(nextCallback: () -> Unit) {
+        runOrPostpone {
+            getFragmentStack()?.push(AlertFragment.create(
+                    pageNumber = "35",
+                    message = context.getString(R.string.fixing_beginning_of_packaging_phase_was_successful),
+                    iconRes = R.drawable.is_warning_yellow_80dp,
+                    isVisibleLeftButton = false,
+                    codeConfirmForRight = backFragmentResultHelper.setFuncForResult(nextCallback),
+                    rightButtonDecorationInfo = ButtonDecorationInfo.next
+            ))
+        }
+    }
 
 }
 
@@ -91,7 +109,8 @@ interface IScreenNavigator : ICoreNavigator {
     fun openLoginScreen()
     fun openFastDataLoadingScreen()
 
-    //fun showConfirmPriceTagsPrinting(priceTagNumber: Int, confirmCallback: () -> Unit)
-    //fun showPrintPriceOffer(goodName: String, noCallback: () -> Unit, yesCallback: () -> Unit)
+    fun showDefrostingPhaseIsCompleted(nextCallback: () -> Unit)
+    fun showConfirmNoSuchItemLeft(confirmCallback: () -> Unit)
+    fun showFixingPackagingPhaseSuccessful(nextCallback: () -> Unit)
 
 }
