feca3d64b3724cf0dc2b8975e2a26a25c107bbae
Maksim Kulikov <maxim.kulikov@osinit.com>
2019-12-02 14:38:33 +0500

    pro - Добавлена кнопка второй ветки

--- production/src/main/java/com/lenta/bp16/features/main_menu/MainMenuViewModel.kt
+++ production/src/main/java/com/lenta/bp16/features/main_menu/MainMenuViewModel.kt
@@ -1,5 +1,7 @@
 package com.lenta.bp16.features.main_menu
 
+import com.lenta.bp16.model.ITaskManager
+import com.lenta.bp16.model.TaskType
 import com.lenta.bp16.platform.navigation.IScreenNavigator
 import com.lenta.shared.platform.viewmodel.CoreViewModel
 import javax.inject.Inject
@@ -8,15 +10,26 @@ class MainMenuViewModel : CoreViewModel() {
 
     @Inject
     lateinit var navigator: IScreenNavigator
+    @Inject
+    lateinit var taskManager: ITaskManager
 
 
     fun onClickAuxiliaryMenu() {
         navigator.openAuxiliaryMenuScreen()
     }
 
+    fun onClickExternalSupply() {
+        taskManager.taskType = TaskType.EXTERNAL_SUPPLY
+        openTaskList()
+    }
+
+    fun onClickProcessingUnit() {
+        taskManager.taskType = TaskType.PROCESSING_UNIT
+        openTaskList()
+    }
+
     fun openTaskList() {
         navigator.openTaskListScreen()
-
     }
 
 }
