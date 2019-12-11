ae1827276201bdc0d6534ca719bd83a0b728ae62
Maksim Kulikov <maxim.kulikov@osinit.com>
2019-12-11 12:29:20 +0500

    pro - Удалены лишние справочники

--- production/src/main/java/com/lenta/bp16/request/FastResourcesMultiRequest.kt
+++ production/src/main/java/com/lenta/bp16/request/FastResourcesMultiRequest.kt
@@ -1,8 +1,7 @@
 package com.lenta.bp16.request
 
-import com.lenta.shared.fmp.resources.fast.*
-import com.lenta.shared.fmp.resources.slow.ZfmpUtz48V001
-import com.lenta.shared.fmp.resources.slow.ZmpUtz25V001
+import com.lenta.shared.fmp.resources.fast.ZmpUtz14V001
+import com.lenta.shared.fmp.resources.fast.ZmpUtz17V001
 import com.lenta.shared.requests.network.CoreResourcesMultiRequest
 import com.mobrun.plugin.api.HyperHive
 import com.mobrun.plugin.api.request_assistant.CustomParameter
@@ -16,15 +15,8 @@ class FastResourcesMultiRequest @Inject constructor(val hyperHive: HyperHive) : 
 
     override fun getMapOfRequests(): Map<String, RequestBuilder<out CustomParameter, out ScalarParameter<Any>>> {
         return mapOf(
-                ZmpUtz07V001.NAME_RESOURCE to ZmpUtz07V001(hyperHive).newRequest(),
                 ZmpUtz14V001.NAME_RESOURCE to ZmpUtz14V001(hyperHive).newRequest(),
-                ZmpUtz17V001.NAME_RESOURCE to ZmpUtz17V001(hyperHive).newRequest(),
-                ZmpUtz23V001.NAME_RESOURCE to ZmpUtz23V001(hyperHive).newRequest(),
-                ZmpUtz38V001.NAME_RESOURCE to ZmpUtz38V001(hyperHive).newRequest(),
-
-                //TODO удалить загрузку SlowData после добавления в FMP sdk возможности обращения к базе во время синхронизации
-                ZfmpUtz48V001.NAME_RESOURCE to ZfmpUtz48V001(hyperHive).newRequest(),
-                ZmpUtz25V001.NAME_RESOURCE to ZmpUtz25V001(hyperHive).newRequest()
+                ZmpUtz17V001.NAME_RESOURCE to ZmpUtz17V001(hyperHive).newRequest()
         )
     }
 
