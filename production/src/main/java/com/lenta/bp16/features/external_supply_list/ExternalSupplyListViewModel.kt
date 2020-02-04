c6f801e40a982d3f8991c9a20cf22a19a678ff1a
Borisenko Denis <borisenkoda@gmail.com>
2020-02-04 20:47:46 +0300

    3332-pro-не-разблокируется-кнопка-завершить

--- production/src/main/java/com/lenta/bp16/features/external_supply_list/ExternalSupplyListViewModel.kt
+++ production/src/main/java/com/lenta/bp16/features/external_supply_list/ExternalSupplyListViewModel.kt
@@ -49,8 +49,13 @@ class ExternalSupplyListViewModel : CoreViewModel() {
     }
 
     val completeEnabled by lazy {
-        task.map { task ->
+        /*task.map { task ->
             task?.isProcessed == false && task.goods?.map { it.getFactRawQuantity() }?.find { it == 0.0 }?.let { false } ?: true
+        }*/
+        task.map { task ->
+            task?.isProcessed == false && task.goods?.map {
+                it.getFactRawQuantity()
+            }?.any { it > 0.0 } ?: false
         }
     }
 
