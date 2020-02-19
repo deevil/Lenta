cb6c2cc612d6c79300724c9147e088d73802af41
Maksim Kulikov <maksim.kulikov@krit.pro>
2020-02-19 20:39:36 +0500

    bks - Информационные экраны

--- back_sales/src/main/java/com/lenta/bp12/platform/navigation/ScreenNavigator.kt
+++ back_sales/src/main/java/com/lenta/bp12/platform/navigation/ScreenNavigator.kt
@@ -63,41 +63,124 @@ class ScreenNavigator @Inject constructor(
     // Информационные экраны
     override fun showUnsentDataFoundOnDevice(deleteCallback: () -> Unit, goOverCallback: () -> Unit) {
         runOrPostpone {
-            getFragmentStack()?.push(AlertFragment.create(message = context.getString(R.string.unsent_data_found_on_device),
+            getFragmentStack()?.push(AlertFragment.create(
                     pageNumber = "89",
+                    message = context.getString(R.string.unsent_data_found_on_device),
                     iconRes = R.drawable.ic_question_80dp,
                     codeConfirmForButton3 = backFragmentResultHelper.setFuncForResult(deleteCallback),
                     codeConfirmForRight = backFragmentResultHelper.setFuncForResult(goOverCallback),
                     buttonDecorationInfo3 = ButtonDecorationInfo.delete,
-                    rightButtonDecorationInfo = ButtonDecorationInfo.goOver))
+                    rightButtonDecorationInfo = ButtonDecorationInfo.goOver
+            ))
         }
     }
 
     override fun showTwelveCharactersEntered(sapCallback: () -> Unit, barCallback: () -> Unit) {
         runOrPostpone {
-            getFragmentStack()?.push(AlertFragment.create(message = context.getString(R.string.twelve_characters_entered),
-                    pageNumber = "40",
+            getFragmentStack()?.push(AlertFragment.create(
+                    pageNumber = "90",
+                    message = context.getString(R.string.twelve_characters_entered),
                     codeConfirmForLeft = backFragmentResultHelper.setFuncForResult(sapCallback),
                     codeConfirmForRight = backFragmentResultHelper.setFuncForResult(barCallback),
                     leftButtonDecorationInfo = ButtonDecorationInfo.sap,
-                    rightButtonDecorationInfo = ButtonDecorationInfo.barcode))
+                    rightButtonDecorationInfo = ButtonDecorationInfo.barcode
+            ))
         }
     }
 
     override fun showUnsavedDataWillBeLost(proceedCallback: () -> Unit) {
         runOrPostpone {
             getFragmentStack()?.push(AlertFragment.create(
+                    pageNumber = "92",
                     message = context.getString(R.string.unsaved_data_will_be_lost),
                     iconRes = R.drawable.ic_question_80dp,
                     codeConfirmForRight = backFragmentResultHelper.setFuncForResult(proceedCallback),
-                    pageNumber = "92",
-                    leftButtonDecorationInfo = ButtonDecorationInfo.back,
                     rightButtonDecorationInfo = ButtonDecorationInfo.proceed
+            ))
+        }
+    }
+
+    override fun showMakeTaskCountedAndClose(yesCallback: () -> Unit) {
+        runOrPostpone {
+            getFragmentStack()?.push(AlertFragment.create(
+                    pageNumber = "94",
+                    message = context.getString(R.string.make_task_counted_and_close),
+                    iconRes = R.drawable.ic_question_80dp,
+                    codeConfirmForRight = backFragmentResultHelper.setFuncForResult(yesCallback),
+                    rightButtonDecorationInfo = ButtonDecorationInfo.yes
+            ))
+        }
+    }
+
+    override fun showTaskUnsentDataWillBeDeleted(taskName: String, applyCallback: () -> Unit) {
+        runOrPostpone {
+            getFragmentStack()?.push(AlertFragment.create(
+                    pageNumber = "95",
+                    message = context.getString(R.string.task_unsent_data_will_be_deleted, taskName),
+                    iconRes = R.drawable.ic_delete_red_80dp,
+                    codeConfirmForRight = backFragmentResultHelper.setFuncForResult(applyCallback),
+                    rightButtonDecorationInfo = ButtonDecorationInfo.apply
+            ))
+        }
+    }
+
+    override fun showScannedMarkBelongsToProduct(productName: String) {
+        runOrPostpone {
+            getFragmentStack()?.push(AlertFragment.create(
+                    pageNumber = "98",
+                    message = context.getString(R.string.scanned_mark_belongs_to_product, productName),
+                    iconRes = R.drawable.is_warning_red_80dp
+            ))
+        }
+    }
+
+    override fun showForExciseGoodNeedScanFirstMark() {
+        runOrPostpone {
+            getFragmentStack()?.push(AlertFragment.create(
+                    pageNumber = "96",
+                    message = context.getString(R.string.for_excise_good_need_scan_first_mark),
+                    iconRes = R.drawable.ic_info_green_80dp,
+                    timeAutoExitInMillis = 2000
+            ))
+        }
+    }
+
+    override fun showRawGoodsRemainedInTask(yesCallback: () -> Unit) {
+        runOrPostpone {
+            getFragmentStack()?.push(AlertFragment.create(
+                    pageNumber = "36",
+                    message = context.getString(R.string.raw_goods_remained_in_task),
+                    iconRes = R.drawable.ic_question_80dp,
+                    codeConfirmForRight = backFragmentResultHelper.setFuncForResult(yesCallback),
+                    rightButtonDecorationInfo = ButtonDecorationInfo.yes
+            ))
+        }
+    }
+
+    override fun showBoxWasLastScanned(afterShowCallback: () -> Unit) {
+        runOrPostpone {
+            getFragmentStack()?.push(AlertFragment.create(
+                    pageNumber = "96",
+                    message = context.getString(R.string.box_was_last_scanned),
+                    iconRes = R.drawable.is_warning_red_80dp,
+                    codeConfirmForExit = backFragmentResultHelper.setFuncForResult(afterShowCallback),
+                    timeAutoExitInMillis = 2000
+            ))
+        }
+    }
+
+    override fun showDoYouReallyWantSetZeroQuantity(yesCallback: () -> Unit, counted: Int) {
+        runOrPostpone {
+            getFragmentStack()?.push(AlertFragment.create(
+                    pageNumber = "65",
+                    message = context.getString(R.string.do_you_really_want_set_zero_quantity, counted),
+                    iconRes = R.drawable.ic_question_80dp,
+                    codeConfirmForRight = backFragmentResultHelper.setFuncForResult(yesCallback),
+                    rightButtonDecorationInfo = ButtonDecorationInfo.yes
             )
             )
         }
     }
-
 
 
 
@@ -113,6 +196,12 @@ interface IScreenNavigator : ICoreNavigator {
 
     fun showUnsentDataFoundOnDevice(deleteCallback: () -> Unit, goOverCallback: () -> Unit)
     fun showTwelveCharactersEntered(sapCallback: () -> Unit, barCallback: () -> Unit)
-    fun showUnsavedDataWillBeLost(yesCallback: () -> Unit)
-
+    fun showUnsavedDataWillBeLost(proceedCallback: () -> Unit)
+    fun showMakeTaskCountedAndClose(yesCallback: () -> Unit)
+    fun showTaskUnsentDataWillBeDeleted(taskName: String, applyCallback: () -> Unit)
+    fun showScannedMarkBelongsToProduct(productName: String)
+    fun showForExciseGoodNeedScanFirstMark()
+    fun showRawGoodsRemainedInTask(yesCallback: () -> Unit)
+    fun showBoxWasLastScanned(afterShowCallback: () -> Unit)
+    fun showDoYouReallyWantSetZeroQuantity(yesCallback: () -> Unit, counted: Int)
 }
