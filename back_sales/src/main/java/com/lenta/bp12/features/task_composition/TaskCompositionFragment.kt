6ec85910a93efdacf3d10cc899b800862bc6e514
Maksim Kulikov <maksim.kulikov@krit.pro>
2020-02-25 14:02:43 +0500

    bks - Состав задания базовая логика

--- back_sales/src/main/java/com/lenta/bp12/features/task_composition/TaskCompositionFragment.kt
+++ back_sales/src/main/java/com/lenta/bp12/features/task_composition/TaskCompositionFragment.kt
@@ -1,21 +1,32 @@
 package com.lenta.bp12.features.task_composition
 
 import com.lenta.bp12.R
-import com.lenta.bp12.databinding.FragmentTaskCompositionBinding
 import com.lenta.shared.platform.fragment.CoreFragment
 import com.lenta.shared.platform.toolbar.bottom_toolbar.BottomToolbarUiModel
 import com.lenta.shared.platform.toolbar.top_toolbar.TopToolbarUiModel
 import com.lenta.shared.utilities.extentions.provideViewModel
 import android.os.Bundle
+import android.view.LayoutInflater
 import com.lenta.shared.utilities.databinding.ViewPagerSettings
 import android.view.ViewGroup
 import android.view.View
+import androidx.databinding.DataBindingUtil
+import com.lenta.bp12.databinding.*
 import com.lenta.bp12.platform.extention.getAppComponent
+import com.lenta.bp12.BR
 import com.lenta.shared.platform.toolbar.bottom_toolbar.ButtonDecorationInfo
+import com.lenta.shared.platform.toolbar.bottom_toolbar.ToolbarButtonsClickListener
+import com.lenta.shared.utilities.databinding.DataBindingAdapter
+import com.lenta.shared.utilities.databinding.DataBindingRecyclerViewConfig
+import com.lenta.shared.utilities.databinding.RecyclerViewKeyHandler
 import com.lenta.shared.utilities.extentions.connectLiveData
 import com.lenta.shared.utilities.extentions.generateScreenNumberFromPostfix
 
-class TaskCompositionFragment : CoreFragment<FragmentTaskCompositionBinding, TaskCompositionViewModel>(), ViewPagerSettings {
+class TaskCompositionFragment : CoreFragment<FragmentTaskCompositionBinding, TaskCompositionViewModel>(),
+        ToolbarButtonsClickListener, ViewPagerSettings {
+
+    private var goodRecyclerViewKeyHandler: RecyclerViewKeyHandler<*>? = null
+    private var basketRecyclerViewKeyHandler: RecyclerViewKeyHandler<*>? = null
 
     override fun getLayoutId(): Int = R.layout.fragment_task_composition
 
@@ -29,7 +40,9 @@ class TaskCompositionFragment : CoreFragment<FragmentTaskCompositionBinding, Tas
     }
 
     override fun setupTopToolBar(topToolbarUiModel: TopToolbarUiModel) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        topToolbarUiModel.description.value = getString(R.string.task_composition)
+
+        connectLiveData(vm.title, topToolbarUiModel.title)
     }
 
     override fun setupBottomToolBar(bottomToolbarUiModel: BottomToolbarUiModel) {
@@ -41,12 +54,101 @@ class TaskCompositionFragment : CoreFragment<FragmentTaskCompositionBinding, Tas
         connectLiveData(vm.saveEnabled, getBottomToolBarUIModel()!!.uiModelButton5.enabled)
     }
 
+    override fun onToolbarButtonClick(view: View) {
+        when (view.id) {
+            R.id.b_3 -> vm.onClickDelete()
+            R.id.b_5 -> vm.onClickSave()
+        }
+    }
+
     override fun getPagerItemView(container: ViewGroup, position: Int): View {
-        return View(context)
+        return when (position) {
+            0 -> getTaskCompositionGoodsView(container)
+            1 -> getTaskCompositionBasketsView(container)
+            else -> View(context)
+        }
+    }
+
+    private fun getTaskCompositionGoodsView(container: ViewGroup): View {
+        DataBindingUtil.inflate<LayoutTaskCompositionGoodsBinding>(LayoutInflater.from(container.context),
+                R.layout.layout_task_composition_goods,
+                container,
+                false).let { layoutBinding ->
+
+            val onClickSelectionListener = View.OnClickListener {
+                (it!!.tag as Int).let { position ->
+                    vm.goodSelectionsHelper.revert(position = position)
+                    layoutBinding.rv.adapter?.notifyItemChanged(position)
+                }
+            }
+
+            layoutBinding.rvConfig = DataBindingRecyclerViewConfig(
+                    layoutId = R.layout.item_task_composition_good,
+                    itemId = BR.item,
+                    realisation = object : DataBindingAdapter<ItemTaskCompositionGoodBinding> {
+                        override fun onCreate(binding: ItemTaskCompositionGoodBinding) {
+                        }
+
+                        override fun onBind(binding: ItemTaskCompositionGoodBinding, position: Int) {
+                            binding.tvItemNumber.tag = position
+                            binding.tvItemNumber.setOnClickListener(onClickSelectionListener)
+                            binding.selectedForDelete = vm.goodSelectionsHelper.isSelected(position)
+                            goodRecyclerViewKeyHandler?.let {
+                                binding.root.isSelected = it.isSelected(position)
+                            }
+                        }
+                    }
+            )
+
+            layoutBinding.vm = vm
+            layoutBinding.lifecycleOwner = viewLifecycleOwner
+            return layoutBinding.root
+        }
+    }
+
+    private fun getTaskCompositionBasketsView(container: ViewGroup): View {
+        DataBindingUtil.inflate<LayoutTaskCompositionBasketsBinding>(LayoutInflater.from(container.context),
+                R.layout.layout_task_composition_baskets,
+                container,
+                false).let { layoutBinding ->
+
+            val onClickSelectionListener = View.OnClickListener {
+                (it!!.tag as Int).let { position ->
+                    vm.basketSelectionsHelper.revert(position = position)
+                    layoutBinding.rv.adapter?.notifyItemChanged(position)
+                }
+            }
+
+            layoutBinding.rvConfig = DataBindingRecyclerViewConfig(
+                    layoutId = R.layout.item_task_composition_basket,
+                    itemId = BR.item,
+                    realisation = object : DataBindingAdapter<ItemTaskCompositionBasketBinding> {
+                        override fun onCreate(binding: ItemTaskCompositionBasketBinding) {
+                        }
+
+                        override fun onBind(binding: ItemTaskCompositionBasketBinding, position: Int) {
+                            binding.tvItemNumber.tag = position
+                            binding.tvItemNumber.setOnClickListener(onClickSelectionListener)
+                            binding.selectedForDelete = vm.basketSelectionsHelper.isSelected(position)
+                            basketRecyclerViewKeyHandler?.let {
+                                binding.root.isSelected = it.isSelected(position)
+                            }
+                        }
+                    }
+            )
+
+            layoutBinding.vm = vm
+            layoutBinding.lifecycleOwner = viewLifecycleOwner
+            return layoutBinding.root
+        }
     }
 
     override fun getTextTitle(position: Int): String {
-        return "Title: $position"
+        return when (position) {
+            0 -> getString(R.string.goods)
+            1 -> getString(R.string.baskets)
+            else -> throw IllegalArgumentException("Wrong pager position!")
+        }
     }
 
     override fun countTab(): Int {
@@ -57,6 +159,5 @@ class TaskCompositionFragment : CoreFragment<FragmentTaskCompositionBinding, Tas
         super.onViewCreated(view, savedInstanceState)
         binding?.viewPagerSettings = this
     }
-
 
 }
--- back_sales/src/main/java/com/lenta/bp12/features/task_composition/TaskCompositionViewModel.kt
+++ back_sales/src/main/java/com/lenta/bp12/features/task_composition/TaskCompositionViewModel.kt
@@ -2,21 +2,42 @@ package com.lenta.bp12.features.task_composition
 
 import com.lenta.shared.platform.viewmodel.CoreViewModel
 import androidx.lifecycle.MutableLiveData
+import com.lenta.bp12.platform.navigation.IScreenNavigator
+import com.lenta.shared.utilities.SelectionItemsHelper
 import com.lenta.shared.utilities.databinding.PageSelectionListener
+import javax.inject.Inject
 
 class TaskCompositionViewModel : CoreViewModel(), PageSelectionListener {
 
+    @Inject
+    lateinit var navigator: IScreenNavigator
+
+
+    val goodSelectionsHelper = SelectionItemsHelper()
+    val basketSelectionsHelper = SelectionItemsHelper()
+
     val selectedPage = MutableLiveData(0)
+
+    val title = MutableLiveData("ВПП // Возврат от 10.12.2018 15:20")
 
     val deleteEnabled = MutableLiveData(false)
 
     val saveEnabled = MutableLiveData(false)
 
+    // -----------------------------
+
     override fun onPageSelected(position: Int) {
         selectedPage.value = position
     }
 
-    // TODO: Implement the ViewModel
+    fun onClickDelete() {
+
+    }
+
+    fun onClickSave() {
+
+    }
+
 }
 
 
