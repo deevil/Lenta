f61879506a1b661ff2d71ae794c9af4fcda0f350
Maksim Kulikov <maksim.kulikov@krit.pro>
2020-03-12 11:01:05 +0500

    bks - Открытие задания - макет и базовая логика

--- back_sales/src/main/java/com/lenta/bp12/features/task_card_open/TaskCardOpenViewModel.kt
+++ back_sales/src/main/java/com/lenta/bp12/features/task_card_open/TaskCardOpenViewModel.kt
@@ -5,15 +5,9 @@ import androidx.lifecycle.viewModelScope
 import com.lenta.bp12.platform.navigation.IScreenNavigator
 import com.lenta.bp12.repository.IDatabaseRepository
 import com.lenta.shared.account.ISessionInfo
-import com.lenta.shared.fmp.resources.dao_ext.TaskType
-import com.lenta.shared.platform.constants.Constants
 import com.lenta.shared.platform.viewmodel.CoreViewModel
 import com.lenta.shared.utilities.databinding.PageSelectionListener
-import com.lenta.shared.utilities.extentions.map
-import com.lenta.shared.view.OnPositionClickListener
 import kotlinx.coroutines.launch
-import java.text.SimpleDateFormat
-import java.util.*
 import javax.inject.Inject
 
 class TaskCardOpenViewModel : CoreViewModel(), PageSelectionListener {
@@ -36,68 +30,26 @@ class TaskCardOpenViewModel : CoreViewModel(), PageSelectionListener {
 
     val selectedPage = MutableLiveData(0)
 
-    val taskName = MutableLiveData("Возврат от ${SimpleDateFormat(Constants.DATE_FORMAT_dd_mm_yyyy_hh_mm, Locale.getDefault()).format(Date())}")
-
     val ui by lazy {
-        TaskCardUi(
-                provider = "568932 ООО Микоян",
+        TaskCardOpenUi(
+                type = "Возврат брака прямому поставщику",
+                name = "Возврат от 05.08.2020 15:16",
+                supplier = "568932 ООО Микоян",
+                storage = "0010",
+                reason = "Нарушение товарного вида",
                 description = "Возврат прямому поставщику",
                 comment = "Комплектование необходимо выполнить до 16:00!!!",
-                isStrict = true,
+                isStrict = false,
                 isAlcohol = false,
                 isCommon = true
         )
-    }
-
-    val taskTypePosition = MutableLiveData(0)
-
-    val reasonForReturnPosition = MutableLiveData(0)
-
-    val storagePosition = MutableLiveData(0)
-
-    val onSelectTaskType = object : OnPositionClickListener {
-        override fun onClickPosition(position: Int) {
-            taskTypePosition.value = position
-        }
-    }
-
-    val onSelectReasonForReturn = object : OnPositionClickListener {
-        override fun onClickPosition(position: Int) {
-            taskTypePosition.value = position
-        }
-    }
-
-    val onSelectStorage = object : OnPositionClickListener {
-        override fun onClickPosition(position: Int) {
-            taskTypePosition.value = position
-        }
-    }
-
-    private val taskTypes = MutableLiveData<List<TaskType>>(emptyList())
-
-    val taskTypeList: MutableLiveData<List<String>> by lazy {
-        taskTypes.map { list ->
-            list?.map { it.description }
-        }
-    }
-
-    val reasonForReturnList: MutableLiveData<List<String>> by lazy {
-        MutableLiveData(List(3) {
-            "Reason for return ${it + 1}"
-        })
-    }
-
-    val storageList: MutableLiveData<List<String>> by lazy {
-        MutableLiveData(List(3) {
-            "Storage ${it + 1}"
-        })
     }
 
     // -----------------------------
 
     init {
         viewModelScope.launch {
-            taskTypes.value = database.getTaskTypeList()
+            //taskTypes.value = database.getTaskTypeList()
         }
     }
 
@@ -113,8 +65,12 @@ class TaskCardOpenViewModel : CoreViewModel(), PageSelectionListener {
 
 }
 
-data class TaskCardUi(
-        val provider: String,
+data class TaskCardOpenUi(
+        val type: String,
+        val name: String,
+        val supplier: String,
+        val storage: String,
+        val reason: String,
         val description: String,
         val comment: String,
         val isStrict: Boolean,
