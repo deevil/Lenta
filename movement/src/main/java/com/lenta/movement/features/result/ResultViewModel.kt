5d2d959f09f3839278fd4d97612c263556eab65d
Vladimir <vladimir.danshin@krit.pro>
2020-07-09 13:41:07 +0300

    Fixed resultfragment

--- movement/src/main/java/com/lenta/movement/features/result/ResultViewModel.kt
+++ movement/src/main/java/com/lenta/movement/features/result/ResultViewModel.kt
@@ -1,7 +1,24 @@
 package com.lenta.movement.features.result
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.lenta.movement.platform.navigation.IScreenNavigator
+import com.lenta.shared.platform.viewmodel.CoreViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class ResultViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class ResultViewModel : CoreViewModel() {
+
+    @Inject
+    lateinit var navigator: IScreenNavigator
+
+    val deviceIp = MutableLiveData("")
+
+    fun onClickComplete(){
+        viewModelScope.launch {
+            navigator.showProgressLoadingData()
+            //TODO Обработать клик
+        }
+    }
 }
