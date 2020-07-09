ced734a292548d50c0f518a9de623f256ed57b44
Vladimir <vladimir.danshin@krit.pro>
2020-07-09 19:23:54 +0300

    ScreenNavigator created

---
+++ open/src/main/java/com/lenta/bp18/di/AppModule.kt
@@ -0,0 +1,78 @@
+package com.lenta.bp18.di
+
+import app_update.AppUpdateInstaller
+import app_update.AppUpdaterConfig
+import app_update.AppUpdaterInstallerFromFmp
+import com.lenta.bp16.BuildConfig.APPLICATION_ID
+import com.lenta.bp16.data.IPrinter
+import com.lenta.bp16.data.IScales
+import com.lenta.bp16.data.Printer
+import com.lenta.bp16.data.Scales
+import com.lenta.bp16.model.IPersistLabelList
+import com.lenta.bp16.model.ITaskManager
+import com.lenta.bp16.model.PersistLabelList
+import com.lenta.bp16.model.TaskManager
+import com.lenta.bp16.platform.navigation.IScreenNavigator
+import com.lenta.bp16.platform.navigation.ScreenNavigator
+import com.lenta.bp16.platform.resource.IResourceManager
+import com.lenta.bp16.platform.resource.ResourceManager
+import com.lenta.bp16.repository.DatabaseRepository
+import com.lenta.bp16.repository.IDatabaseRepository
+import com.lenta.bp16.repository.IRepoInMemoryHolder
+import com.lenta.bp16.repository.RepoInMemoryHolder
+import com.lenta.shared.di.AppScope
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+
+@Module(includes = [AppModule.Declarations::class])
+class AppModule {
+
+    @Module
+    internal interface Declarations {
+
+        @Binds
+        @AppScope
+        fun bindAppUpdateInstaller(realisation: AppUpdaterInstallerFromFmp): AppUpdateInstaller
+
+        @Binds
+        @AppScope
+        fun bindRepoInMemoryHolder(realisation: RepoInMemoryHolder): IRepoInMemoryHolder
+
+        @Binds
+        @AppScope
+        fun bindScreenNavigator(realisation: ScreenNavigator): IScreenNavigator
+
+        @Binds
+        @AppScope
+        fun bindGeneralRepository(realisation: DatabaseRepository): IDatabaseRepository
+
+        @Binds
+        @AppScope
+        fun bindTaskManager(realisation: TaskManager): ITaskManager
+
+        @Binds
+        @AppScope
+        fun bindResourceManager(realisation: ResourceManager): IResourceManager
+
+        @Binds
+        @AppScope
+        fun bindScales(realisation: Scales): IScales
+
+        @Binds
+        @AppScope
+        fun bindPrinter(realisation: Printer): IPrinter
+
+        @Binds
+        @AppScope
+        fun bindPersistTaskData(realisation: PersistLabelList): IPersistLabelList
+
+    }
+
+    @Provides
+    @AppScope
+    internal fun provideAppUpdaterConfig(): AppUpdaterConfig {
+        return AppUpdaterConfig(folderName = "bp16", applicationId = APPLICATION_ID)
+    }
+
+}
--- open/src/main/java/com/lenta/bp18/platform/navigation/ScreenNavigator.kt
+++ open/src/main/java/com/lenta/bp18/platform/navigation/ScreenNavigator.kt
@@ -1,2 +1,72 @@
 package com.lenta.bp18.platform.navigation
 
+import android.content.Context
+import com.lenta.bp18.features.auth.AuthFragment
+import com.lenta.bp18.features.goods_info.GoodsInfoFragment
+import com.lenta.bp18.features.result.ResultFragment
+import com.lenta.bp18.features.search.SearchFragment
+import com.lenta.bp18.features.select_market.SelectMarketFragment
+import com.lenta.bp18.features.sync.SyncFragment
+import com.lenta.shared.account.IAuthenticator
+import com.lenta.shared.platform.activity.ForegroundActivityProvider
+import com.lenta.shared.platform.navigation.ICoreNavigator
+import com.lenta.shared.platform.navigation.runOrPostpone
+import com.lenta.shared.progress.IProgressUseCaseInformator
+import javax.inject.Inject
+
+class ScreenNavigator @Inject constructor(
+        private val context: Context,
+        private val coreNavigator: ICoreNavigator,
+        private val foregroundActivityProvider: ForegroundActivityProvider,
+        private val authenticator: IAuthenticator,
+        private val progressUseCaseInformator: IProgressUseCaseInformator
+) : IScreenNavigator, ICoreNavigator by coreNavigator {
+
+    private fun getFragmentStack() = foregroundActivityProvider.getActivity()?.fragmentStack
+
+    override fun openAuthScreen() {
+        runOrPostpone {
+            getFragmentStack()?.push(AuthFragment())
+        }
+    }
+
+    override fun openSelectMarketScreen() {
+        runOrPostpone {
+            getFragmentStack()?.push(SelectMarketFragment())
+        }
+    }
+
+    override fun openGoodsInfoScreen() {
+        runOrPostpone {
+            getFragmentStack()?.push(GoodsInfoFragment())
+        }
+    }
+
+    override fun openResultScreen() {
+        runOrPostpone {
+            getFragmentStack()?.push(ResultFragment())
+        }
+    }
+
+    override fun openSearchScreen() {
+        runOrPostpone {
+            getFragmentStack()?.push(SearchFragment())
+        }
+    }
+
+    override fun openSyncScreen() {
+        runOrPostpone {
+            getFragmentStack()?.push(SyncFragment())
+        }
+    }
+
+}
+
+interface IScreenNavigator : ICoreNavigator {
+    fun openAuthScreen()
+    fun openSelectMarketScreen()
+    fun openGoodsInfoScreen()
+    fun openResultScreen()
+    fun openSearchScreen()
+    fun openSyncScreen()
+}
