d942ded2ec1f68662f25d22a2350178355987874
Vladimir <vladimir.danshin@krit.pro>
2020-07-13 15:44:25 +0300

    test #2

--- production/src/main/java/com/lenta/bp16/platform/Constants.kt
+++ production/src/main/java/com/lenta/bp16/platform/Constants.kt
@@ -3,7 +3,7 @@ package com.lenta.bp16.platform
 object Constants {
 
     const val BP = "16"
-    //
+    //test #2
     const val TIME_OUT_IN_SEC = 60
     const val SELECT_GOODS_FRAGMENT = "53"
     const val GOODS_INFO_FRAGMENT = "06"
