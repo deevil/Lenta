20d7aa466177ca1d36357262d2dde52445cdc35f
Dima Denisenko <dendima80@gmail.com>
2020-07-13 17:10:03 +0300

    https://trello.com/c/lqyZlYQu 3. Алкоголь. Кор.учет. Карточка товара. Учет брака

--- Shared/src/main/java/com/lenta/shared/utilities/extentions/MutableListExt.kt
+++ Shared/src/main/java/com/lenta/shared/utilities/extentions/MutableListExt.kt
@@ -3,8 +3,7 @@ package com.lenta.shared.utilities.extentions
 fun <T> MutableList<T>.removeItemFromListWithPredicate(
         predicate: (T) -> Boolean
 ): Boolean {
-    val localBad = this
-    localBad.filter { stamp ->
+    this.map { it }.filter { stamp ->
         if (predicate(stamp)) {
             this.remove(stamp)
             return@filter true
