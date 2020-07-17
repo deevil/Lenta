f04b8bbb03bd41d9bba871a79d44ab9830f1a7f9
Alexander <sphc@yandex.ru>
2020-07-17 12:01:01 +0300

    added laaoyut ingredient

---
+++ movement/build/generated/source/buildConfig/prod/debug/com/lenta/movement/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.lenta.movement;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.lenta.movement";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "prod";
+  public static final int VERSION_CODE = 2795;
+  public static final String VERSION_NAME = ".0.8d6479aa";
+}
---
+++ movement/build/generated/source/dataBinding/trigger/prod/debug/com/lenta/movement/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package com.lenta.movement;
+
+import androidx.databinding.BindingBuildInfo;
+
+@BindingBuildInfo
+public class DataBindingInfo {}
