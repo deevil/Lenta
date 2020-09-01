d2c0240fe12abd4ae38b1149fba6e2c00301dac5
vladimir.danshin <vladimir.danshin@krit.pro>
2020-09-01 11:31:37 +0300

    Spinner

--- Shared/src/main/java/com/lenta/shared/utilities/databinding/Spinner.kt
+++ Shared/src/main/java/com/lenta/shared/utilities/databinding/Spinner.kt
@@ -50,29 +50,49 @@ fun setupSpinner(spinner: Spinner, items: List<String>?, position: Int?, onPosit
     adapter.notifyDataSetChanged()
 }
 
-@BindingAdapter(value = ["onPositionClicked", "selectedPosition"], requireAll = false)
-fun onPositionClicked(spinner: Spinner, handlerLiveData: MutableLiveData<Int>?, selectedPosition: Int? = null) {
+@BindingAdapter(value = ["listItems", "selectedPosition", "android:enabled"], requireAll = false)
+fun onPositionClicked(spinner: Spinner, listItems: List<String>?, selectedPosition: MutableLiveData<Int>?, enabled: Boolean?) {
 
-    if (selectedPosition != null && spinner.selectedItemPosition != selectedPosition) {
-        spinner.setSelection(selectedPosition)
+    val selectedPositionValue = selectedPosition?.value
+
+    var adapter: ArrayAdapter<String>
+    if (spinner.adapter == null) {
+        val mutableList: MutableList<String> = listItems.orEmpty().toMutableList()
+        spinner.tag = mutableList
+        adapter = ArrayAdapter(spinner.context, android.R.layout.simple_spinner_item, mutableList)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
     }
 
-    if (spinner.onItemSelectedListener == null && handlerLiveData != null) {
+    if (spinner.onItemSelectedListener == null && selectedPosition != null) {
         spinner.postDelayed({
             spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
                 override fun onItemSelected(adapterView: AdapterView<*>, view: View?, position: Int, l: Long) {
-                    handlerLiveData.value = position
+                    selectedPosition.value = position
                 }
-
                 override fun onNothingSelected(adapterView: AdapterView<*>) = Unit
             }
         }, 500)
-    } else if (handlerLiveData == null) {
+    } else if (selectedPosition == null) {
         spinner.onItemSelectedListener = null
     }
 
     @Suppress("UNCHECKED_CAST")
-    val adapter = (spinner.adapter as ArrayAdapter<String>)
+    adapter = (spinner.adapter as ArrayAdapter<String>)
+    adapter.clear()
+    listItems?.let {
+        adapter.addAll(it)
+        val isEnabled = enabled ?: (it.size > 1)
+        spinner.isEnabled = isEnabled
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            spinner.focusable = if (isEnabled) View.FOCUSABLE else View.NOT_FOCUSABLE
+        }
+    }
+
+
+    if (selectedPositionValue != null && spinner.selectedItemPosition != selectedPositionValue) {
+        spinner.setSelection(selectedPositionValue)
+    }
 
     adapter.notifyDataSetChanged()
 }
