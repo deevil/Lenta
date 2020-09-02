1b07993515d81ceb62186e08b404193361b33b21
evgeniya.nasrtdinova <evgeniya.nasrtdinova@krit.pro>
2020-09-02 19:43:34 +0300

    Исправление отображения списка  после частичного удаления товаров

--- Shared/src/main/java/com/lenta/shared/utilities/SelectionItemsHelper.kt
+++ Shared/src/main/java/com/lenta/shared/utilities/SelectionItemsHelper.kt
@@ -7,13 +7,12 @@ class SelectionItemsHelper {
     val selectedPositions = MutableLiveData(mutableSetOf<Int>())
 
     fun add(position: Int) {
-        selectedPositions.value!!.add(position)
+        selectedPositions.value?.add(position)
         notifyChanged()
     }
 
-
     fun remove(position: Int) {
-        selectedPositions.value!!.remove(position)
+        selectedPositions.value?.remove(position)
         notifyChanged()
     }
 
@@ -25,24 +24,26 @@ class SelectionItemsHelper {
     }
 
     fun clearPositions() {
-        selectedPositions.value!!.clear()
+        selectedPositions.value?.clear()
         notifyChanged()
     }
 
     fun isSelectedEmpty(): Boolean {
-        return selectedPositions.value!!.isEmpty()
+        return selectedPositions.value?.isEmpty() ?: true
     }
 
     fun revert(position: Int) {
-        if (selectedPositions.value!!.contains(position)) {
-            remove(position)
-        } else {
-            add(position)
+        selectedPositions.value?.let { selectedPositionsValue ->
+            if (selectedPositionsValue.contains(position)) {
+                remove(position)
+            } else {
+                add(position)
+            }
         }
     }
 
     fun isSelected(position: Int): Boolean {
-        return selectedPositions.value!!.contains(position)
+        return selectedPositions.value?.contains(position) ?: false
     }
 
     private fun notifyChanged() {
