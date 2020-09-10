94c9446edef42905f838d0ea1c1b9a690c9a5c33
Alexander <sphc@yandex.ru>
2020-09-10 14:20:58 +0300

    fix abstractions over VM

---
+++ production/src/main/java/com/lenta/bp16/features/add_attribute/IZpartVisibleConditions.kt
@@ -0,0 +1,37 @@
+package com.lenta.bp16.features.add_attribute
+
+import com.lenta.bp16.model.ProducerDataStatus
+import com.lenta.shared.utilities.orIfNull
+
+interface IZpartVisibleConditions : IZpartInfo {
+
+    val producerConditions: Pair<Boolean, Boolean>
+        get() {
+            return zPartDataInfo.value?.let { zPartDataInfoValue ->
+
+                val producersList = zPartDataInfoValue.map { it.prodName.orEmpty() }
+
+                var fullItemCount = 0
+                for (zPartName in producersList) {
+                    if (zPartName.isNotEmpty()) {
+                        fullItemCount++ //Считаем количество не пустых полей в списке
+                    }
+                }
+
+                val visibleStatus = when {
+                    (fullItemCount == 0) -> ProducerDataStatus.GONE
+                    (fullItemCount == producersList.size) -> ProducerDataStatus.VISIBLE
+                    else -> ProducerDataStatus.ALERT
+                }
+
+                return when (visibleStatus) {
+                    ProducerDataStatus.GONE -> false to false
+                    ProducerDataStatus.VISIBLE -> true to false
+                    ProducerDataStatus.ALERT -> true to true
+                }
+
+            }.orIfNull {
+                false to false
+            }
+        }
+}
