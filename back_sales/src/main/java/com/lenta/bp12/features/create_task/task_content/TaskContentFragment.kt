0028fe343109d8f72c564ea7a80c31f61756437a
Nail Asadullin <nail.asadullin@krit.pro>
2020-09-14 11:59:28 +0300

    изменения после code review 5

--- back_sales/src/main/java/com/lenta/bp12/features/create_task/task_content/TaskContentFragment.kt
+++ back_sales/src/main/java/com/lenta/bp12/features/create_task/task_content/TaskContentFragment.kt
@@ -16,7 +16,6 @@ import com.lenta.shared.platform.toolbar.bottom_toolbar.ButtonDecorationInfo
 import com.lenta.shared.platform.toolbar.bottom_toolbar.ToolbarButtonsClickListener
 import com.lenta.shared.platform.toolbar.top_toolbar.TopToolbarUiModel
 import com.lenta.shared.scan.OnScanResultListener
-import com.lenta.shared.utilities.databinding.RecyclerViewKeyHandler
 import com.lenta.shared.utilities.databinding.ViewPagerSettings
 import com.lenta.shared.utilities.extentions.connectLiveData
 import com.lenta.shared.utilities.extentions.generateScreenNumberFromPostfix
