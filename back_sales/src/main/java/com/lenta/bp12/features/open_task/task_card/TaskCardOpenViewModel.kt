bacf4fdbeff70a87d7f25fe23cc29f86266446ea
Nail Asadullin <nail.asadullin@krit.pro>
2020-09-30 14:52:56 +0300

    task card open refactor

--- back_sales/src/main/java/com/lenta/bp12/features/open_task/task_card/TaskCardOpenViewModel.kt
+++ back_sales/src/main/java/com/lenta/bp12/features/open_task/task_card/TaskCardOpenViewModel.kt
@@ -15,6 +15,8 @@ import com.lenta.shared.utilities.Logg
 import com.lenta.shared.utilities.databinding.PageSelectionListener
 import com.lenta.shared.utilities.extentions.launchUITryCatch
 import com.lenta.shared.utilities.extentions.map
+import com.lenta.shared.utilities.extentions.mapSkipNulls
+import com.lenta.shared.utilities.extentions.unsafeLazy
 import com.lenta.shared.utilities.orIfNull
 import javax.inject.Inject
 
@@ -57,31 +59,61 @@ class TaskCardOpenViewModel : CoreViewModel(), PageSelectionListener {
         manager.currentTask
     }
 
-    val ui by lazy {
-        task.map {
-            it?.let { task ->
-                val provider = task.getProviderCodeWithName().takeIf { codeWithName ->
-                    codeWithName.isNotEmpty()
-                } ?: resource.wholesaleBuyer()
+    private val ui by lazy {
+        task.mapSkipNulls { task ->
+            val provider = task.getProviderCodeWithName().takeIf { codeWithName ->
+                codeWithName.isNotEmpty()
+            } ?: resource.wholesaleBuyer()
 
-                TaskCardOpenUi(
-                        name = task.name,
-                        provider = provider,
-                        storage = task.storage,
-                        reason = task.reason?.description.orEmpty(),
-                        description = task.type?.description.orEmpty(),
-                        comment = task.comment,
-                        isStrict = task.isStrict,
-                        isAlcohol = task.controlTypes.contains(ControlType.ALCOHOL),
-                        isCommon = task.controlTypes.contains(ControlType.COMMON),
-                        isMark = task.controlTypes.contains(ControlType.MARK)
-                )
-            }
+            TaskCardOpenUi(
+                    name = task.name,
+                    provider = provider,
+                    storage = task.storage,
+                    reason = task.reason?.description.orEmpty(),
+                    description = task.type?.description.orEmpty(),
+                    comment = task.comment,
+                    isStrict = task.isStrict,
+                    isAlcohol = task.controlTypes.contains(ControlType.ALCOHOL),
+                    isCommon = task.controlTypes.contains(ControlType.COMMON),
+                    isMark = task.controlTypes.contains(ControlType.MARK)
+            )
         }
     }
 
-    val isReasonIsEmpty = ui.map {
-        it?.reason?.isEmpty() == true
+    val reason by unsafeLazy {
+        ui.mapSkipNulls {
+            it.reason
+        }
+    }
+
+    val isReasonIsNotEmpty by unsafeLazy {
+        ui.mapSkipNulls {
+            it.reason.isNotEmpty()
+        }
+    }
+
+    val storage by unsafeLazy {
+        ui.mapSkipNulls {
+            it.storage
+        }
+    }
+
+    val provider by unsafeLazy {
+        ui.mapSkipNulls {
+            it.provider
+        }
+    }
+
+    val name by unsafeLazy {
+        ui.mapSkipNulls {
+            it.name
+        }
+    }
+
+    val description by unsafeLazy {
+        ui.mapSkipNulls {
+            it.description
+        }
     }
 
     val isExistComment by lazy {
@@ -200,5 +232,4 @@ class TaskCardOpenViewModel : CoreViewModel(), PageSelectionListener {
         /** Режим работы: 1 - получение состава задания, 2 - получение состава задания с переблокировкой */
         private const val GET_GOOD_LIST_MODE = 1
     }
-
 }
