ef9dc6ce7c989ee1a720f953513ed2648e6169d9
Dima Denisenko <dendima80@gmail.com>
2020-10-01 11:19:57 +0300

    bug barcode parser

--- Shared/src/main/java/com/lenta/shared/platform/viewmodel/BarcodeScannedParser.kt
+++ Shared/src/main/java/com/lenta/shared/platform/viewmodel/BarcodeScannedParser.kt
@@ -3,6 +3,7 @@ package com.lenta.shared.platform.viewmodel
 import com.lenta.shared.models.core.BarcodeData
 import com.lenta.shared.models.core.BarcodeInfo
 import com.lenta.shared.models.core.Batch
+import com.lenta.shared.utilities.Logg
 import com.lenta.shared.utilities.extentions.unsafeLazy
 import com.lenta.shared.utilities.gs1.*
 import kotlinx.coroutines.Dispatchers
@@ -20,69 +21,75 @@ open class BarcodeScannedParser {
         var weight = DEFAULT_ZERO_VALUE
         var batch: Batch? = null
 
-        when {
-            // SAP-код (только ручной ввод)
-            barcodeLength in 0..MAX_SAP_CODE_LENGTH -> {
-                if (GTIN.convertibleToGTIN8(data)) {
-                    barcode = GTIN.toGTIN8(data)
-                }
-                isWeight = GTIN.isVariableMeasureItem(barcode)
-                if (isWeight) {
-                    val changedBarcode = getChangedBarcodeAndWeight(barcode)
-                    barcode = changedBarcode.first
-                    weight = changedBarcode.second
-                }
-            }
-            //EAN (ручной ввод и сканирование)
-            barcodeLength in MAX_SAP_CODE_LENGTH..MAX_EAN_CODE_LENGTH -> {
-                isWeight = getIsWeightFromCode(barcode)
-                if (isWeight) {
-                    val changedBarcode = getChangedBarcodeAndWeight(barcode)
-                    barcode = changedBarcode.first
-                    weight = changedBarcode.second
-                }
-            }
-            // парсинг кодов GS1 с разделителями
-            barcodeLength >= MINIMUM_GS1_CODE_LENGTH -> {
-                val parsedEntities = EAN128Parser.parse(barcode, false)
-                if (parsedEntities.isNotEmpty()) {
-                    barcode = parsedEntities.getString(ApplicationIdentifier.GTIN)
+        try {
+            when {
+                // SAP-код (только ручной ввод)
+                barcodeLength in 0..MAX_SAP_CODE_LENGTH -> {
+                    if (GTIN.convertibleToGTIN8(data)) {
+                        barcode = GTIN.toGTIN8(data)
+                    }
                     isWeight = GTIN.isVariableMeasureItem(barcode)
                     if (isWeight) {
                         val changedBarcode = getChangedBarcodeAndWeight(barcode)
                         barcode = changedBarcode.first
                         weight = changedBarcode.second
-                    } else {
-                        weight = parsedEntities.getString(ApplicationIdentifier.ITEM_NET_WEIGHT_KG).takeIf { it.isNotEmpty() }
-                                ?: DEFAULT_ZERO_VALUE
-                    }
-
-                    val batchNumber = parsedEntities.getString(ApplicationIdentifier.BATCH_OR_LOT_NUMBER)
-                    val count = parsedEntities.getString(ApplicationIdentifier.COUNT_OF_TRADE_ITEMS)
-                    val dateProduction = parsedEntities.getDate(ApplicationIdentifier.PRODUCTION_DATE_AND_TIME)
-                    val dateExpiration = parsedEntities.getDate(ApplicationIdentifier.EXPIRATION_DATE_AND_TIME)
-
-                    batch = Batch(batchNumber, count, dateProduction, dateExpiration)
-                } else {
-                    val result = ElementStrings.parse(barcode)
-                    if (!result.isEmpty) {
-                        barcode = result.getString(ApplicationIdentifier.GTIN)
-                        weight = result.getString(ApplicationIdentifier.ITEM_NET_WEIGHT_KG).takeIf { it.isNotEmpty() }
-                                ?: DEFAULT_ZERO_VALUE
-                        isWeight = GTIN.isVariableMeasureItem(barcode)
-
-                        val batchNumber = result.getString(ApplicationIdentifier.BATCH_OR_LOT_NUMBER)
-                        val count = result.getString(ApplicationIdentifier.VARIABLE_COUNT)
-                        val dateProduction = result.getDate(ApplicationIdentifier.PRODUCTION_DATE)
-                        val dateTimeExpiration = result.getDate(ApplicationIdentifier.EXPIRATION_DATE_AND_TIME)
-
-                        batch = Batch(batchNumber, count, dateProduction, dateTimeExpiration)
                     }
                 }
-                if (barcode.isNotEmpty() && barcode.first() == DEFAULT_LEADING_ZERO) {
-                    barcode = barcode.substring(1 until barcode.length)
+                //EAN (ручной ввод и сканирование)
+                barcodeLength in MAX_SAP_CODE_LENGTH..MAX_EAN_CODE_LENGTH -> {
+                    isWeight = getIsWeightFromCode(barcode)
+                    if (isWeight) {
+                        val changedBarcode = getChangedBarcodeAndWeight(barcode)
+                        barcode = changedBarcode.first
+                        weight = changedBarcode.second
+                    }
+                }
+                // парсинг кодов GS1 с разделителями
+                barcodeLength >= MINIMUM_GS1_CODE_LENGTH -> {
+                    val parsedEntities = EAN128Parser.parse(barcode, false)
+                    if (parsedEntities.isNotEmpty()) {
+                        barcode = parsedEntities.getString(ApplicationIdentifier.GTIN)
+                        isWeight = GTIN.isVariableMeasureItem(barcode)
+                        if (isWeight) {
+                            val changedBarcode = getChangedBarcodeAndWeight(barcode)
+                            barcode = changedBarcode.first
+                            weight = changedBarcode.second
+                        } else {
+                            weight = parsedEntities.getString(ApplicationIdentifier.ITEM_NET_WEIGHT_KG).takeIf { it.isNotEmpty() }
+                                    ?: DEFAULT_ZERO_VALUE
+                        }
+
+                        val batchNumber = parsedEntities.getString(ApplicationIdentifier.BATCH_OR_LOT_NUMBER)
+                        val count = parsedEntities.getString(ApplicationIdentifier.COUNT_OF_TRADE_ITEMS)
+                        val dateProduction = parsedEntities.getDate(ApplicationIdentifier.PRODUCTION_DATE_AND_TIME)
+                        val dateExpiration = parsedEntities.getDate(ApplicationIdentifier.EXPIRATION_DATE_AND_TIME)
+
+                        batch = Batch(batchNumber, count, dateProduction, dateExpiration)
+                    } else {
+                        val result = ElementStrings.parse(barcode)
+                        if (!result.isEmpty) {
+                            barcode = result.getString(ApplicationIdentifier.GTIN)
+                            weight = result.getString(ApplicationIdentifier.ITEM_NET_WEIGHT_KG).takeIf { it.isNotEmpty() }
+                                    ?: DEFAULT_ZERO_VALUE
+                            isWeight = GTIN.isVariableMeasureItem(barcode)
+
+                            val batchNumber = result.getString(ApplicationIdentifier.BATCH_OR_LOT_NUMBER)
+                            val count = result.getString(ApplicationIdentifier.VARIABLE_COUNT)
+                            val dateProduction = result.getDate(ApplicationIdentifier.PRODUCTION_DATE)
+                            val dateTimeExpiration = result.getDate(ApplicationIdentifier.EXPIRATION_DATE_AND_TIME)
+
+                            batch = Batch(batchNumber, count, dateProduction, dateTimeExpiration)
+                        }
+                    }
+                    if (barcode.first() == DEFAULT_LEADING_ZERO) {
+                        barcode = barcode.substring(1 until barcode.length)
+                    }
                 }
             }
+
+
+        } catch (e: Exception) {
+            Logg.e { "error BarcodeScannedParser: $e" }
         }
 
         BarcodeData(
