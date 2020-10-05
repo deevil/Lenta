3eb6f7f5b6717ccebaf38d4610638ac79d1122bb
Maksim Kulikov <kulikovman@gmail.com>
2020-10-05 14:54:56 +0500

    https://trello.com/c/4Y7HceRF - Поиск заданий

--- statement_on_balance/src/main/java/com/lenta/bp15/features/search_task/SearchTaskViewModel.kt
+++ statement_on_balance/src/main/java/com/lenta/bp15/features/search_task/SearchTaskViewModel.kt
@@ -1,10 +1,16 @@
 package com.lenta.bp15.features.search_task
 
 import androidx.lifecycle.MutableLiveData
+import com.lenta.bp15.model.ITaskManager
+import com.lenta.bp15.model.enum.ShoesMarkType
 import com.lenta.bp15.platform.navigation.IScreenNavigator
 import com.lenta.bp15.platform.resource.IResourceManager
+import com.lenta.bp15.repository.requests.pojo.TaskSearchParams
 import com.lenta.shared.account.ISessionInfo
 import com.lenta.shared.platform.viewmodel.CoreViewModel
+import com.lenta.shared.utilities.actionByNumber
+import com.lenta.shared.utilities.extentions.launchUITryCatch
+import com.lenta.shared.utilities.extentions.map
 import javax.inject.Inject
 
 class SearchTaskViewModel : CoreViewModel() {
@@ -18,12 +24,19 @@ class SearchTaskViewModel : CoreViewModel() {
     @Inject
     lateinit var resource: IResourceManager
 
+    @Inject
+    lateinit var manager: ITaskManager
+
+
+    /**
+    Переменные
+     */
 
     val title by lazy {
         resource.tk(sessionInfo.market.orEmpty())
     }
 
-    val search = MutableLiveData("")
+    val number = MutableLiveData("")
 
     val requestFocusToSearch = MutableLiveData(false)
 
@@ -35,14 +48,45 @@ class SearchTaskViewModel : CoreViewModel() {
 
     val isUnisex = MutableLiveData(false)
 
-    val searchEnabled = MutableLiveData(false)
+    val searchEnabled = number.map { it?.isNotEmpty() }
+
+    /**
+    Методы
+     */
+
+    fun clearAllMarkType() {
+        isMan.value = false
+        isWoman.value = false
+        isChildren.value = false
+        isUnisex.value = false
+    }
 
     fun onClickSearch() {
+        launchUITryCatch {
+            manager.loadSearchTaskList(TaskSearchParams(
+                    material = number.value.orEmpty(),
+                    markType = getMarkType()
+            ))
 
+            navigator.goBack()
+        }
+    }
+
+    private fun getMarkType(): String {
+        return when {
+            isMan.value == true -> ShoesMarkType.MAN.code
+            isWoman.value == true -> ShoesMarkType.WOMAN.code
+            isChildren.value == true -> ShoesMarkType.CHILDREN.code
+            isUnisex.value == true -> ShoesMarkType.UNISEX.code
+            else -> ShoesMarkType.UNKNOWN.code
+        }
     }
 
     fun onScanResult(data: String) {
-
+        actionByNumber(
+                number = data,
+                funcForNotValidFormat = navigator::showIncorrectEanFormat
+        )
     }
 
 }
--- statement_on_balance/src/main/java/com/lenta/bp15/model/enum/ShoesMarkType.kt
+++ statement_on_balance/src/main/java/com/lenta/bp15/model/enum/ShoesMarkType.kt
@@ -1,12 +1,12 @@
 package com.lenta.bp15.model.enum
 
-enum class ShoesMarkType(val description: String) {
+enum class ShoesMarkType(val code: String, val description: String) {
 
-    UNKNOWN("Unknown type"),
-    MAN("Обувь мужская"),
-    WOMAN("Обувь женская"),
-    CHILDREN("Обувь детская"),
-    UNISEX("Обувь унисекс");
+    UNKNOWN("","Unknown type"),
+    MAN("M","Обувь мужская"),
+    WOMAN("F","Обувь женская"),
+    CHILDREN("C","Обувь детская"),
+    UNISEX("O","Обувь унисекс");
 
     companion object {
         fun from(code: String): ShoesMarkType {
